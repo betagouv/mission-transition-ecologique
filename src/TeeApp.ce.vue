b098813e3fc08c9eb76c0d8f9dd8f2a2f803fa95
Julien Paris <jparis.py@gmail.com>
2023-05-09 19:09:37 +0200

    debugging css imports

--- src/TeeApp.ce.vue
+++ src/TeeApp.ce.vue
@@ -110,11 +110,11 @@
 // cf : https://stackoverflow.com/questions/71163741/vuejs-script-setup-cannot-contain-es-module-exports
 
 import '@gouvfr/dsfr/dist/core/core.main.min.css'            // Le CSS minimal du DSFR
-import '@gouvfr/dsfr/dist/component/component.main.min.css'  // Styles de tous les composants du DSFR
-import '@gouvfr/dsfr/dist/utility/utility.main.min.css'      // Classes utilitaires : les composants de VueDsfr en ont besoin
-import '@gouvminint/vue-dsfr/styles'                         // Les styles propres aux composants de VueDsfr
-import '@gouvfr/dsfr/dist/scheme/scheme.min.css'             // Facultatif : Si les thèmes sont utilisés (thème sombre, thème clair)
-import '@gouvfr/dsfr/dist/utility/icons/icons.min.css'       // Facultatif : Si des icônes sont utilisées avec les classes "fr-icon-..."
+// import '@gouvfr/dsfr/dist/component/component.main.min.css'  // Styles de tous les composants du DSFR
+// import '@gouvfr/dsfr/dist/utility/utility.main.min.css'      // Classes utilitaires : les composants de VueDsfr en ont besoin
+// import '@gouvminint/vue-dsfr/styles'                         // Les styles propres aux composants de VueDsfr
+// import '@gouvfr/dsfr/dist/scheme/scheme.min.css'             // Facultatif : Si les thèmes sont utilisés (thème sombre, thème clair)
+// import '@gouvfr/dsfr/dist/utility/icons/icons.min.css'       // Facultatif : Si des icônes sont utilisées avec les classes "fr-icon-..."
 
 import { ref, onBeforeMount } from 'vue'
 
@@ -211,9 +211,10 @@ onBeforeMount(() => {
 </style> -->
 
 <style lang="scss">
-  @import '~@gouvfr/dsfr/dist/dsfr.min.css'; // ok
+  // @import '../public/core.main.css';
   // @import '~@gouvfr/dsfr/dist/core/core.main.min.css';
-  // @import '../public/css/core.main.min.css';
+
+  @import '~@gouvfr/dsfr/dist/dsfr.min.css'; // ok
 
   // @import '~@gouvfr/dsfr/dist/dsfr.legacy.min.css';
   // @import '~@gouvfr/dsfr/dist/dsfr.main.min.css';
@@ -222,8 +223,8 @@ onBeforeMount(() => {
   // @import '~@gouvfr/dsfr/dist/utility/utility.main.min.css';
 
   // @import '~@gouvfr/dsfr/dist/scheme/scheme.min.css';
-  // @import '~@gouvfr/dsfr/dist/utility/icons/icons.min.css'; // ok
+  @import '~@gouvfr/dsfr/dist/utility/icons/icons.min.css'; // ok
 
   // @import '~@gouvminint/vue-dsfr/styles';
-  // @import '~@gouvminint/vue-dsfr/dist/vue-dsfr.css'; // ok
+  @import '~@gouvminint/vue-dsfr/dist/vue-dsfr.css'; // ok
 </style>
