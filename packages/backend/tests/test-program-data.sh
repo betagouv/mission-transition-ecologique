9d0b594f28f03d5d7e4d7fbaa85f04cb74a760e5
Pierre Camilleri <22995923+pierrecamilleri@users.noreply.github.com>
2023-09-04 13:56:14 +0200

    🔵 use npx to be able to run the script without npm

--- packages/backend/tests/test-program-data.sh
+++ packages/backend/tests/test-program-data.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # Script that tests all program data files against a schema.
-# Do not run manually, use `npm run test-data` instead (in backend workspace).
+# Can be run via `npm run test-data` (in backend workspace).
 
-find ../web/public/data/programs -type f -print0 | xargs -0 -I {} -n1 ajv test --spec=draft2020 -s data/program-data-schema.json -d {}  --valid
+find ../web/public/data/programs -type f -print0 | xargs -0 -I {} -n1 npx ajv test --spec=draft2020 -s data/program-data-schema.json -d {}  --valid
