adffab297d818431cb840f01afa926fdbd29b44e
Pierre Camilleri <22995923+pierrecamilleri@users.noreply.github.com>
2023-09-04 13:59:16 +0200

    🔵 extract path into variable

--- packages/backend/tests/test-program-data.sh
+++ packages/backend/tests/test-program-data.sh
@@ -2,4 +2,8 @@
 # Script that tests all program data files against a schema.
 # Can be run via `npm run test-data` (in backend workspace).
 
-find ../web/public/data/programs -type f -print0 | xargs -0 -I {} -n1 npx ajv test --spec=draft2020 -s data/program-data-schema.json -d {}  --valid
+PROGRAMS_DATA_DIR=../web/public/data/programs
+JSON_SCHEMA_FILE=data/program-data-schema.json
+
+find "${PROGRAMS_DATA_DIR}" -type f -print0 | \
+  xargs -0 -I {} -n1 npx ajv test --spec=draft2020 -s "${JSON_SCHEMA_FILE}" -d {}  --valid
