ef08ce23e6f1e57e775d04dde18c999bb29aa4e8
Pierre Camilleri <22995923+pierrecamilleri@users.noreply.github.com>
2023-09-04 13:49:23 +0200

    fix: Only test files (and not dirs)

--- packages/backend/tests/test-program-data.sh
+++ packages/backend/tests/test-program-data.sh
@@ -2,4 +2,4 @@
 # Script that tests all program data files against a schema.
 # Do not run manually, use `npm run test-data` instead (in backend workspace).
 
-find ../web/public/data/programs -print0 | xargs -0 -I {} -n1 ajv test --spec=draft2020 -s data/program-data-schema.json -d {}  --valid
+find ../web/public/data/programs -type f -print0 | xargs -0 -I {} -n1 ajv test --spec=draft2020 -s data/program-data-schema.json -d {}  --valid
