2da2f578052d6a410d52cb2bb2c2749a77755a60
Pierre Camilleri <22995923+pierrecamilleri@users.noreply.github.com>
2023-09-08 16:22:12 +0200

    🔵 remove dataset output
    Unsure if still works on deploy, needs manual testing

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -16,6 +16,47 @@ console.log('vite.config > mode : ', mode)
 const rawEnv = loadEnv(mode, process.cwd())
 console.log('vite.config > rawEnv : ', rawEnv)
 
+/*
+// Build programs dataset from folder and yaml files
+// Parse data folder to build list of programs
+// Each program must must written as a distinct yaml file
+// cf : https://stackoverflow.com/questions/10049557/reading-all-files-in-a-directory-store-them-in-objects-and-send-the-object
+*/
+// requiring path and fs modules
+import * as path from 'path'
+import * as fs from 'fs'
+import * as yaml from 'js-yaml'
+const programsArray = <any[]>[]
+// joining path of directory
+const dataDirPathTemp = rawEnv.VITE_DATA_DIR_PATH || '../data/programs'
+const dataDirPath = path.join(__dirname, dataDirPathTemp)
+// console.log('vite.config > __dirname :', __dirname)
+console.log('vite.config > dataDirPath :', dataDirPath)
+
+// passsing dataDiryPath and callback function
+const filesNames = fs.readdirSync(dataDirPath)
+console.log('vite.config > filesNames :', filesNames)
+filesNames.forEach((file) => {
+  // Do whatever you want to do with the file
+  console.log()
+  console.log('vite.config > file :', file)
+  const yamlFilePath = `${dataDirPath}/${file}`
+  const yamlFile = fs.readFileSync(yamlFilePath, 'utf8')
+  const yamlObj = yaml.load(yamlFile) || {}
+  // yamlObj.file = file
+  // @ts-ignore
+  console.log('vite.config > yamlObj.title :', yamlObj.title)
+  programsArray.push(yamlObj)
+})
+// console.log('vite.config > programsArray :', programsArray)
+
+// build output json
+// const dataAsJson = JSON.stringify(programsArray, null, 2)
+// const dataBuiltOutput = './public/data/output/dataset_out.json'
+// const dataOutPath = path.join(__dirname, dataBuiltOutput)
+// fs.writeFileSync(dataOutPath, dataAsJson)
+// console.log('vite.config > finished writing output json...')
+
 // VITE CONFIG
 const viteServer = {
   // host: 'localhost',
