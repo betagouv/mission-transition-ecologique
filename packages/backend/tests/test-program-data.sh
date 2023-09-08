02bee2cccdee83f591380dce1fc373b4fdec4516
Pierre Camilleri <22995923+pierrecamilleri@users.noreply.github.com>
2023-09-08 11:17:26 +0200

    🔵 document sh script

--- packages/backend/tests/test-program-data.sh
+++ packages/backend/tests/test-program-data.sh
@@ -5,5 +5,12 @@
 PROGRAMS_DATA_DIR=../web/public/data/programs
 JSON_SCHEMA_FILE=data/program-data-schema.json
 
+# find options:
+#  -print0 : use null delimiter, so that xargs -0 can loop on the array
+# xargs options:
+#  -I {}   : use {} as filename placeholder to be used by ajv with "-d {}"
+#  -n1     : run the command on each single filename
+# See [ajv documentation](https://ajv.js.org/guide/getting-started.html) for
+# details on the command line validator.
 find "${PROGRAMS_DATA_DIR}" -type f -print0 | \
   xargs -0 -I {} -n1 npx ajv test --spec=draft2020 -s "${JSON_SCHEMA_FILE}" -d {}  --valid
