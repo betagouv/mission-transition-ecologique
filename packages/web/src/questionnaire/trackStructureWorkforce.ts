86c76e10569fd311503747e198bbd57ab3ca0eaa
Pierre Camilleri <22995923+pierrecamilleri@users.noreply.github.com>
2023-09-29 16:59:26 +0200

    feat: filtering programs with publicodes.  (#128)
    This PR introduces changes into how the filtering occurs : 
    
    - data contains now a `publicodes` attribute that defines rules for
    filtering.
    - Currently, one mandatory rule `éligibilité`
    - The rules can only use data given by the front-end (company and
    questionnaire), mostly `entreprise . effectif` by now.
    - Rules that cannot be evaluated because of missing data result in an
    error. This behavior will probably change in the future, but is
    currently a security that mistakes don't go unnoticed.
    
    Some details : 
    - `structure_workforce` is renamed `entreprise . effectif`, and
    conditions related to `structure_workforce` are removed from data.
    - Other conditions (unrelated to `entreprise . effectif`) are still
    evaluated as before
    - `filterPrograms` is tested with `npm run test -w packages/backend`
    - Types will need a bit of work in the future, for instead the
    `InputData` type is pretty uninformative right now.
    
    Plus : 
    - Version bump of true-myth to benefit from [this bug
    fix](https://github.com/true-myth/true-myth/pull/608)

--- packages/web/src/questionnaire/trackStructureWorkforce.ts
+++ packages/web/src/questionnaire/trackStructureWorkforce.ts
@@ -2,43 +2,43 @@ const nextExceptions = [
   {
     help: "Goes to track_sectors if : doesn't have infos about sector",
     rules: [
-      { 
+      {
         from: 'usedTracks',
         id: 'siret',
         dataField: 'siret',
         conditions: [
-          { 
+          {
             type: 'siret',
             operator: 'inexists'
           }
         ]
-      },
+      }
     ],
-    next: { default: 'track_sectors'}
+    next: { default: 'track_sectors' }
   },
   {
     help: "Goes to track_structure_building_property if : has infos about sector AND user_help == 'unknown' (newbie)",
     rules: [
-      { 
+      {
         from: 'usedTracks',
         id: 'project_sectors',
         dataField: 'project_sectors',
         conditions: [
-          { 
+          {
             type: 'project_sectors',
             operator: 'exists'
           }
         ]
       },
-      { 
+      {
         from: 'usedTracks',
         id: 'user_help',
         dataField: 'user_help',
         conditions: [
-          { 
+          {
             type: 'user_help',
             operator: '==',
-            value: 'unknown',
+            value: 'unknown'
           }
         ]
       }
@@ -48,33 +48,33 @@ const nextExceptions = [
   {
     help: "Goes to track_goals if : have infos about sector AND user_help == 'precise' (pro)",
     rules: [
-      { 
+      {
         from: 'usedTracks',
         id: 'project_sectors',
         dataField: 'project_sectors',
         conditions: [
-          { 
+          {
             type: 'project_sectors',
             operator: 'exists'
           }
         ]
       },
-      { 
+      {
         from: 'usedTracks',
         id: 'user_help',
         dataField: 'user_help',
         conditions: [
-          { 
+          {
             type: 'user_help',
             operator: '==',
-            value: 'precise',
+            value: 'precise'
           }
         ]
       }
     ],
     next: { default: 'track_goals' }
   }
-] 
+]
 
 export const workforce = {
   id: 'track_structure_workforce',
@@ -82,21 +82,21 @@ export const workforce = {
   title: { fr: 'Mes effectifs' },
   label: { fr: 'Combien êtes-vous dans votre entreprise ?' },
   interface: {
-    component: 'buttons',
+    component: 'buttons'
   },
   behavior: {
-    multipleChoices: false,
+    multipleChoices: false
   },
   options: [
     {
-      value: { structure_workforce: 0, structure_sizes: ['PME', 'TPE'] },
-      title: { fr: "TPE ou PME" },
-      label: { fr: "🧍‍♂employé.s" },
+      value: { 'entreprise . effectif': 0, structure_sizes: ['PME', 'TPE'] },
+      title: { fr: 'TPE ou PME' },
+      label: { fr: '🧍‍♂employé.s' },
       hasInput: 'number',
       defaultInput: 1,
       inputMax: 249,
       inputMin: 1,
-      inputField: 'structure_workforce',
+      inputField: 'entreprise . effectif',
       inputCleaning: [
         {
           operator: '<',
@@ -118,9 +118,9 @@ export const workforce = {
     },
     {
       disabled: false,
-      value: { structure_workforce: '+250', structure_sizes: ['ETI', 'GE'] },
+      value: { 'entreprise . effectif': 251, structure_sizes: ['ETI', 'GE'] },
       title: { fr: '+250 employés' },
-      label: { fr: "‍️👫👭 Plus de 250 employés" },
+      label: { fr: '‍️👫👭 Plus de 250 employés' },
       next: {
         default: 'track_sectors',
         exceptions: nextExceptions
