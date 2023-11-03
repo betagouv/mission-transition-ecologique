9e81fd2980d1361b176e54e42d6b14eb746c444e
Julien Paris <jparis.py@gmail.com>
2023-11-03 12:41:41 +0100

    fix: changes in vite config and packages scripts to adapt to new paradigm of deployment

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -44,7 +44,7 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html')
+        app: resolve(__dirname, 'index.html')
       }
     },
     outDir: 'dist',
