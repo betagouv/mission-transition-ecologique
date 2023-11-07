f5cbcc9672e95c4b62401ba756dff86a1904d67b
Julien Paris <jparis.py@gmail.com>
2023-11-07 08:05:14 +0100

    refactor: simpler way to template TeeHomeSteps + responsive on this component

--- packages/web/src/components/home/TeeHomeSteps.vue
+++ packages/web/src/components/home/TeeHomeSteps.vue
@@ -1,46 +1,62 @@
 <template>
   <div
     class="fr-grid-row fr-grid-row-gutters">
-    <div class="fr-col fr-col-4 fr-pt-8v fr-text-center fr-pb-0">
-      <img class="fr-mb-5v" src="/images/home/1.svg" alt="1" />
-      <h3>
-        Je renseigne mon profil avec mon SIRET
-      </h3>
-    </div>
-    <div class="fr-col fr-col-4 fr-pt-8v fr-text-center fr-pb-0">
-      <img class="fr-mb-5v" src="/images/home/2.svg" alt="2" />
-      <h3>
-        Je réponds à quelques questions par thématique
-      </h3>
-    </div>
-    <div class="fr-col fr-col-4 fr-pt-8v fr-text-center fr-pb-0">
-      <img class="fr-mb-5v" src="/images/home/3.svg" alt="3" />
-      <h3>
-        J’accède aux dispositifs de soutien pour mon entreprise
-      </h3>
+    <div
+      v-for="c in content"
+      :key="`${c.img}-title`"
+      class="fr-col fr-col-xs-12 fr-col-md-4 fr-pt-8v fr-text-center fr-pb-0">
+      <div class="fr-grid-row">
+        <div class="fr-col fr-col-xs-2 fr-col-sm-12">
+          <img 
+            class="fr-mb-5v" 
+            :src="c.img" 
+            :alt="c.title"
+            />
+        </div>
+        <div class="fr-col fr-col-xs-10 fr-col-sm-12">
+          <h3>
+            {{ c.title }}
+          </h3>
+        </div>
+      </div>
     </div>
   </div>
   <div
-    class="fr-grid-row fr-grid-row-gutters">
-    <div class="fr-col fr-col-4 fr-pb-8v fr-pt-0 fr-text-center">
+    class="fr-grid-row fr-grid-row-gutters fr-sm-hide">
+    <div
+      v-for="c in content"
+      :key="`${c.img}-text`"
+      class="fr-col fr-col-4 fr-pb-8v fr-pt-0 fr-text-center">
       <p>
-        pour obtenir en quelques clics des résultats personnalisés 
-        éligible dans votre secteur d’activité et géographique.
-      </p>
-    </div>
-    <div class="fr-col fr-col-4 fr-pb-8v fr-pt-0 fr-text-center">
-      <p>
-        En fonction de vos objectifs et des enjeux de votre entreprise : 
-        bâtiment, mobilité, déchets, eau, énergie...
-      </p>
-    </div>
-    <div class="fr-col fr-col-4 fr-pb-8v fr-pt-0 fr-text-center">
-      <p>
-        Des propositions d’accompagnements et aides financières pour 
-        financer vos projets issues 
-        de l’ensemble des partenaires :
-        ADEME, BPI, CCI et CMA !
+        {{ c.text }}
       </p>
     </div>
   </div>
 </template>
+
+<script setup lang="ts">
+
+const content = [
+  {
+    title: 'Je renseigne mon profil avec mon SIRET',
+    img: '/images/home/1.svg',
+    text: "pour obtenir en quelques clics des résultats personnalisés \
+          éligible dans votre secteur d’activité et géographique."
+  },
+  {
+    title: 'Je réponds à quelques questions par thématique',
+    img: '/images/home/2.svg',
+    text: " En fonction de vos objectifs et des enjeux de votre entreprise : \
+          bâtiment, mobilité, déchets, eau, énergie..."
+  },
+  {
+    title: "J’accède aux dispositifs de soutien pour mon entreprise",
+    img: '/images/home/3.svg',
+    text: " Des propositions d’accompagnements et aides financières pour \
+          financer vos projets issues \
+          de l’ensemble des partenaires :\
+          ADEME, BPI, CCI et CMA !"
+  }
+]
+
+</script>
