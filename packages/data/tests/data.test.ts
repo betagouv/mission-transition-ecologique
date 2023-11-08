3fd578faa3ef68ae4b553bc6b0b16f6191d64d70
Pierre Camilleri <22995923+pierrecamilleri@users.noreply.github.com>
2023-11-08 14:02:00 +0100

    test: Add schema + test to check region name consistency

--- packages/data/tests/data.test.ts
+++ packages/data/tests/data.test.ts
@@ -1,16 +1,18 @@
 import Ajv from 'ajv/dist/2020'
-import schema from '../schemas/program-data-schema.json'
+import programSchema from '../schemas/program-data-schema.json'
+import regionSchema from '../schemas/region-data-schema.json'
+import regionData from '../../web/public/data/references/com_codes.json'
 
 import { prependInterface, readPrograms } from '../src/dataPipeline'
 
 import Engine from 'publicodes'
 
 test('JSON Schema is valid', () => {
-  expect(new Ajv().compile(schema)).not.toThrowError()
+  expect(new Ajv().compile(programSchema)).not.toThrowError()
 })
 
 test('Data is valid against the JSON schema', () => {
-  const validate = new Ajv().compile(schema)
+  const validate = new Ajv({ verbose: true }).compile(programSchema)
 
   const programs = readPrograms()
 
@@ -34,4 +36,16 @@ test('Publicode data is valid when appended with interface', () => {
   programs.forEach((p) => {
     expect(() => new Engine(p.publicodes as any)).not.toThrowError()
   })
+})
+
+test('Region data from the passage table is consistent with what is expected in the publicodes interface', () => {
+  const validate = new Ajv({ verbose: true }).compile(regionSchema)
+  const valid = validate(regionData)
+
+  if (!valid) {
+    console.log('Region data is not valid')
+    console.log(validate.errors)
+  }
+
+  expect(valid).toBe(true)
 })
