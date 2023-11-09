265e46dfb0227d00307b36b9e725b498f0b9f130
Pierre Camilleri <22995923+pierrecamilleri@users.noreply.github.com>
2023-11-09 16:43:47 +0100

    Link mobility question to objective

--- packages/web/src/questionnaire/trackMobilityNumberVehicles.ts
+++ packages/web/src/questionnaire/trackMobilityNumberVehicles.ts
@@ -1,43 +1,52 @@
 import type { Track } from '@/types'
-import { TrackComponents, TrackId } from '@/types'
+import { TrackComponents, TrackId, YesNo, Objectives } from '@/types'
 
 export const mobilityNumberVehicles: Track = {
   id: TrackId.MobilityNumberVehicles,
   category: 'myMobility',
   title: { fr: 'Nombre de véhicules' },
-  label: { fr: "Possédez-vous un ou plusieurs véhicules motorisés dans votre entreprise ?" },
+  label: { fr: 'Possédez-vous un ou plusieurs véhicules motorisés dans votre entreprise ?' },
   interface: {
-    component: TrackComponents.Buttons,
+    component: TrackComponents.Buttons
   },
   behavior: {
-    multipleChoices: false,
+    multipleChoices: false
   },
   next: {
-    default: TrackId.MobilityEnergy,
+    default: TrackId.MobilityEnergy
   },
   options: [
     {
-      value: { mobility_number_vehicles: 'yes' },
+      value: {
+        mobility_number_vehicles: 'yes',
+        [Objectives.SustainableMobility]: YesNo.Yes
+      },
       title: { fr: 'Oui' },
-      label: { fr: "✅ Oui" },
+      label: { fr: '✅ Oui' },
       next: {
-        default: TrackId.MobilityEnergy,
+        default: TrackId.MobilityEnergy
       }
     },
     {
-      value: { mobility_number_vehicles: 'no' },
-      title: { fr: "Non" },
-      label: { fr: "❌ Non, l’entreprise ne possède aucun véhicule motorisé" },
+      value: {
+        mobility_number_vehicles: 'no',
+        [Objectives.SustainableMobility]: YesNo.No
+      },
+      title: { fr: 'Non' },
+      label: { fr: '❌ Non, l’entreprise ne possède aucun véhicule motorisé' },
       next: {
-        default: TrackId.WastesStake,
+        default: TrackId.WastesStake
       }
     },
     {
-      value: { mobility_number_vehicles: 'unknown' },
+      value: {
+        mobility_number_vehicles: 'unknown',
+        [Objectives.SustainableMobility]: YesNo.Yes
+      },
       title: { fr: 'Je ne sais pas' },
-      label: { fr: "Je ne sais pas" },
+      label: { fr: 'Je ne sais pas' },
       next: {
-        default: TrackId.WastesStake,
+        default: TrackId.WastesStake
       }
     }
   ]
