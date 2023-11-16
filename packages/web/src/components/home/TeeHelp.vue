a6dc759e01fcb95270e6cff8d04967b9594d9c9e
Julien Paris <jparis.py@gmail.com>
2023-11-16 12:41:31 +0100

    fix: change email in TeeHelp

--- packages/web/src/components/home/TeeHelp.vue
+++ packages/web/src/components/home/TeeHelp.vue
@@ -5,11 +5,24 @@
         <div class="fr-col-12">
           <div class="fr-follow__newsletter">
             <div>
-              <h5 class="fr-h5 fr-follow__title">Nous avons besoin de vous !</h5>
-              <p class="fr-text--sm fr-follow__desc">Une aide financière qui manque sur la plateforme ? Des bugs que vous aimeriez remonter ? Faites-nous vos retours pour nous aider à améliorer notre outil.</p>
+              <h5 class="fr-h5 fr-follow__title">
+                Nous avons besoin de vous !
+              </h5>
+              <p class="fr-text--sm fr-follow__desc">
+                Une aide financière qui manque sur la plateforme ?
+                Des bugs que vous aimeriez remonter ?
+                Faites-nous vos retours pour nous aider à améliorer
+                notre outil.
+              </p>
             </div>
             <div>
-              <a title="Envoyer un email - ouvre une nouvelle fenêtre" href="mailto:mission-transition-ecologique@developpement-durable.gouv.fr" target="_blank" rel="noopener">Nous contacter</a>
+              <a
+                title="Envoyer un email - ouvre une nouvelle fenêtre"
+                href="mailto:france-transition@beta.gouv.fr"
+                target="_blank"
+                rel="noopener">
+                Nous contacter
+              </a>
             </div>
           </div>
         </div>
