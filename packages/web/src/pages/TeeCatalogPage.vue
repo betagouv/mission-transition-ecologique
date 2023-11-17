c914e4e7fe38d27142658ee47f4ac3d5cc1ad052
Jpy <jparis.py@gmail.com>
2023-11-17 11:04:54 +0100

    Fix/remove banner bottom from pages (#285)
    solves #267

--- packages/web/src/pages/TeeCatalogPage.vue
+++ packages/web/src/pages/TeeCatalogPage.vue
@@ -1,16 +1,9 @@
 <template>
-  <!-- MAIN APP COMPONENT  -->
-  <div
-    class="fr-container--fluid fr-px-20v fr-pb-20v fr-mt-10v fr-mb-20v"
-    style="min-height: 800px">
-    <router-view/>
-  </div>
-  <TeeContact/>
+  <TeeQuestionnairePage/>
 </template>
 
 <script setup lang="ts">
 
 // @ts-ignore
-import TeeContact from '@/components/home/TeeContact.vue'
-
+import TeeQuestionnairePage from './TeeQuestionnairePage.vue';
 </script>
--- packages/web/src/pages/TeeQuestionnairePage.vue
+++ packages/web/src/pages/TeeQuestionnairePage.vue
@@ -5,13 +5,27 @@
     style="min-height: 800px">
     <router-view/>
   </div>
-
-  <TeeContact />
+  <!-- SHOW TeeContact ONLY IF ON RESULTS PAGE -->
+  <TeeContact
+    v-if="isTrackResults"/>
 </template>
 
 <script setup lang="ts">
 
+import { computed } from 'vue'
+
+import { tracksStore } from '../stores/tracks'
+
+// @ts-ignore
+import type { Track } from '@/types/index'
 // @ts-ignore
 import TeeContact from '@/components/home/TeeContact.vue'
 
+const tracks = tracksStore()
+
+// computed
+const isTrackResults = computed(() => {
+  const track: Track | any = tracks.getLastTrack
+  return track?.component === 'results'
+})
 </script>
