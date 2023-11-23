cbf68af11824e2d1e83768baace8a6f8aa608e6c
Jpy <jparis.py@gmail.com>
2023-11-23 15:46:55 +0100

    fix: back to top after every question, but back to last position after coming back from detailled program view (#325)
    solves #294

--- packages/web/src/utils/global.ts
+++ packages/web/src/utils/global.ts
@@ -9,9 +9,9 @@ export const deployMode = metaEnv.MODE != 'development'
 export const deployUrl = metaEnv.VITE_DEPLOY_URL
 export const noDebugSwitch = metaEnv.VITE_NO_DEBUG_SWITCH === 'true'
 export const publicPath = `${deployUrl}/${metaEnv.BASE_URL}`
-console.log('WidgetApp - deployUrl :', deployUrl)
-console.log('WidgetApp - metaEnv.BASE_URL :', metaEnv.BASE_URL)
-console.log('WidgetApp - publicPath :', publicPath)
+// console.log('WidgetApp - deployUrl :', deployUrl)
+// console.log('WidgetApp - metaEnv.BASE_URL :', metaEnv.BASE_URL)
+// console.log('WidgetApp - publicPath :', publicPath)
 
 // @ts-ignore
 export const programsFromJson = jsonDataset
