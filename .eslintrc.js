fc6037fa8502feb92f88c4997a609e6babc8d894
Yohann Valentin <pro@yohannvalentin.com>
2023-11-24 12:07:35 +0100

    chore: WIP

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,11 +5,16 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-type-checked',
+    "prettier"
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     tsconfigRootDir: __dirname,
     project: ['./tsconfig.eslint.json', './packages/*/tsconfig.json'],
   },
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', "prettier"],
+  rules: {
+    "prettier/prettier": "error",
+    "@typescript-eslint/no-unused-vars": "error",
+  }
 };
