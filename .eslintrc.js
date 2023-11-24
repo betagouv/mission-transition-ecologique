63e2d67c04b674f236ee46b38bbd32a0a114e22f
Yohann Valentin <pro@yohannvalentin.com>
2023-11-24 11:33:08 +0100

    chore: WIP

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+/* eslint-env node */
+module.exports = {
+  root: true,
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.eslint.json', './packages/*/tsconfig.json'],
+  },
+  plugins: ['@typescript-eslint'],
+};
