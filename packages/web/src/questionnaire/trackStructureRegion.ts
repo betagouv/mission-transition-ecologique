a4de842d122198510bde510d045a8fc182356076
Julien Paris <jparis.py@gmail.com>
2023-11-27 17:28:38 +0100

    refacto: simpler array for regions + typo

--- packages/web/src/questionnaire/trackStructureRegion.ts
+++ packages/web/src/questionnaire/trackStructureRegion.ts
@@ -10,36 +10,36 @@ import type { NextTrackRuleSet } from '@/types'
 // note : based on authorized values in "/packages/data/common/interface.yaml"
 const regionsList = [
   //France métropolitaine
-  { label: 'Auvergne-Rhône-Alpes' },
-  { label: 'Bourgogne-Franche-Comté' },
-  { label: 'Bretagne' },
-  { label: 'Centre-Val de Loire' },
-  { label: 'Corse' },
-  { label: 'Grand Est' },
-  { label: 'Hauts-de-France' },
-  { label: 'Normandie' },
-  { label: 'Nouvelle-Aquitaine' },
-  { label: 'Occitanie' },
-  { label: "Provence-Alpes-Côte d'Azur" },
-  { label: 'Pays de la Loire' },
-  { label: 'Île-de-France' },
+  'Auvergne-Rhône-Alpes',
+  'Bourgogne-Franche-Comté',
+  'Bretagne',
+  'Centre-Val de Loire',
+  'Corse',
+  'Grand Est',
+  'Hauts-de-France',
+  'Normandie',
+  'Nouvelle-Aquitaine',
+  'Occitanie',
+  "Provence-Alpes-Côte d'Azur",
+  'Pays de la Loire',
+  'Île-de-France',
 
   //DOM
-  { label: 'Guadeloupe' },
-  { label: 'Guyane' },
-  { label: 'La Réunion' },
-  { label: 'Martinique' },
-  { label: 'Mayotte' },
+  'Guadeloupe',
+  'Guyane',
+  'La Réunion',
+  'Martinique',
+  'Mayotte',
 
   //Collectivités d'outre-mer
-  { label: 'Ile de Clipperton' },
-  { label: 'Nouvelle-Calédonie' },
-  { label: 'Polynésie française' },
-  { label: 'Saint-Barthélemy' },
-  { label: 'Saint-Martin' },
-  { label: 'Saint-Pierre-Et-Miquelon' },
-  { label: 'Terres australes et antarctiques françaises' },
-  { label: 'Wallis et Futuna' }
+  'Ile de Clipperton',
+  'Nouvelle-Calédonie',
+  'Polynésie française',
+  'Saint-Barthélemy',
+  'Saint-Martin',
+  'Saint-Pierre-Et-Miquelon',
+  'Terres australes et antarctiques françaises',
+  'Wallis et Futuna'
 ]
 
 const nextExceptions: NextTrackRuleSet[] = [
@@ -62,7 +62,7 @@ const nextExceptions: NextTrackRuleSet[] = [
     next: { default: TrackId.BuildingProperty }
   },
   {
-    help: "Goes to track_goals if : user_help == 'preise' (pro)",
+    help: "Goes to track_goals if : user_help == 'precise' (pro)",
     rules: [
       {
         from: DataMappingFrom.UsedTracks,
@@ -81,11 +81,11 @@ const nextExceptions: NextTrackRuleSet[] = [
   }
 ]
 
-const regionsOptions = regionsList.map(reg => {
+const regionsOptions = regionsList.map(regName => {
   return {
-    value: { région: reg.label },
-    title: { fr: reg.label },
-    label: { fr: reg.label },
+    value: { région: regName },
+    title: { fr: regName },
+    label: { fr: regName },
     next: {
       default: TrackId.Goals,
       exceptions: nextExceptions
