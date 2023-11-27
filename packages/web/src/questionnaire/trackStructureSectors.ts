1e0ef3c9e3f70a2572c3aacbebdac4a136f87681
Julien Paris <jparis.py@gmail.com>
2023-11-27 17:23:30 +0100

    refactor: remove commented code

--- packages/web/src/questionnaire/trackStructureSectors.ts
+++ packages/web/src/questionnaire/trackStructureSectors.ts
@@ -16,45 +16,6 @@ import type {
   // NextTrackRuleSet
 } from '@/types'
 
-// const nextExceptions: NextTrackRuleSet[] = [
-//   {
-//     help: "Goes to track_structure_building_property if : user_help == 'unknown' (newbie)",
-//     rules: [
-//       {
-//         from: DataMappingFrom.UsedTracks,
-//         id: 'user_help',
-//         dataField: 'user_help',
-//         conditions: [
-//           {
-//             type: 'user_help',
-//             operator: ConditionOperators.is,
-//             value: 'unknown'
-//           }
-//         ]
-//       }
-//     ],
-//     next: { default: TrackId.BuildingProperty }
-//   },
-//   {
-//     help: "Goes to track_goals if : user_help == 'precise' (pro)",
-//     rules: [
-//       {
-//         from: DataMappingFrom.UsedTracks,
-//         id: 'user_help',
-//         dataField: 'user_help',
-//         conditions: [
-//           {
-//             type: 'user_help',
-//             operator: ConditionOperators.is,
-//             value: 'precise'
-//           }
-//         ]
-//       }
-//     ],
-//     next: { default: TrackId.Goals }
-//   }
-// ]
-
 export const sectors: Track = {
   id: TrackId.Sectors,
   help: 'https://www.insee.fr/fr/metadonnees/nafr2',
@@ -79,9 +40,7 @@ export const sectors: Track = {
       title: { fr: 'Artisanat' },
       label: { fr: '👩‍🎨 J’ai une activité artisanale' },
       next: {
-        // default: TrackId.Roles,
-        default: TrackId.StructureRegion,
-        // exceptions: nextExceptions
+        default: TrackId.StructureRegion
       }
     },
     {
@@ -95,9 +54,7 @@ export const sectors: Track = {
       title: { fr: 'Industrie' },
       label: { fr: '👩‍🔧 J’ai une activité industrielle, fabrication, production' },
       next: {
-        // default: TrackId.Roles,
-        default: TrackId.StructureRegion,
-        // exceptions: nextExceptions
+        default: TrackId.StructureRegion
       }
     },
     {
@@ -111,9 +68,7 @@ export const sectors: Track = {
       title: { fr: 'Tourisme' },
       label: { fr: '🤵‍♂️ J’ai une activité de tourisme, restauration' },
       next: {
-        // default: TrackId.Roles,
-        default: TrackId.StructureRegion,
-        // exceptions: nextExceptions
+        default: TrackId.StructureRegion
       }
     },
     {
@@ -127,9 +82,7 @@ export const sectors: Track = {
       title: { fr: 'Tertiaire' },
       label: { fr: '🧑‍⚖️ J’ai une activité tertiaire, de services' },
       next: {
-        // default: TrackId.Roles,
-        default: TrackId.StructureRegion,
-        // exceptions: nextExceptions
+        default: TrackId.StructureRegion
       }
     },
     {
@@ -143,9 +96,7 @@ export const sectors: Track = {
       title: { fr: 'Agriculture' },
       label: { fr: '👩‍🌾 J’ai une activité agricole' },
       next: {
-        // default: TrackId.Roles,
-        default: TrackId.StructureRegion,
-        // exceptions: nextExceptions
+        default: TrackId.StructureRegion
       }
     },
     {
@@ -159,9 +110,7 @@ export const sectors: Track = {
       title: { fr: 'Autre' },
       label: { fr: "Je suis dans un autre secteur d'activité" },
       next: {
-        // default: TrackId.Roles,
-        default: TrackId.StructureRegion,
-        // exceptions: nextExceptions
+        default: TrackId.StructureRegion
       }
     }
   ]
