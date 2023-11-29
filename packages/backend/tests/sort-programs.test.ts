6a0c22eab993c489ac646bf0e0252ebf22d6fede
Pierre Camilleri <22995923+pierrecamilleri@users.noreply.github.com>
2023-11-29 18:29:13 +0100

    🟢 dummy test (1 program)
    passes right away

--- packages/backend/tests/sort-programs.test.ts
+++ packages/backend/tests/sort-programs.test.ts
@@ -1,5 +1,6 @@
 import { sortPrograms } from '@tee/backend/src/domain/sort-programs'
 import { ProgramData } from '@tee/web/src/types'
+import { makeProgram } from './testing'
 
 describe(`
  GIVEN a list of programs
@@ -9,5 +10,9 @@ EXPECT that the programs respect a set of given rules
   test('', () => {
     const programs: ProgramData[] = []
     expect(sortPrograms(programs)).toHaveLength(0)
+
+    const program = makeProgram('')
+    const programs2: ProgramData[] = [program]
+    expect(sortPrograms(programs2)[0]).toBe(program)
   })
 })
