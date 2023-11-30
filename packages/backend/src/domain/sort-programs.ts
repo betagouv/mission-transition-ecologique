c70e78c2fb3a60255f65152314bca33db9e7a3b1
Pierre Camilleri <22995923+pierrecamilleri@users.noreply.github.com>
2023-11-30 09:14:33 +0100

    🟢

--- packages/backend/src/domain/sort-programs.ts
+++ packages/backend/src/domain/sort-programs.ts
@@ -1,5 +1,18 @@
 import { ProgramData } from '@tee/web/src/types'
 
 export const sortPrograms = (programs: ProgramData[]): ProgramData[] => {
-  return programs
+  const sortedPrograms = programs.sort((prog1, prog2) => {
+    if (
+      prog1["coût de l'accompagnement"] == 'gratuit' &&
+      prog2["coût de l'accompagnement"] != 'gratuit'
+    )
+      return -1
+    if (
+      prog1["coût de l'accompagnement"] != 'gratuit' &&
+      prog2["coût de l'accompagnement"] == 'gratuit'
+    )
+      return 1
+    return 0
+  })
+  return sortedPrograms
 }
