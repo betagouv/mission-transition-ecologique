c53ece46b6fdb5078dedf6264326ce87c20d128a
Julien Paris <jparis.py@gmail.com>
2023-12-13 18:04:35 +0100

    refactor: test to implement object-curly-newline

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,22 +3,24 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended-type-checked', 'prettier'],
-  plugins: ['@typescript-eslint', 'prettier'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended-type-checked', 'plugin:prettier/recommended'],
+  plugins: ['@typescript-eslint'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     tsconfigRootDir: __dirname,
     project: ['./tsconfig.eslint.json', './packages/*/tsconfig.json', './packages/*/tsconfig.*.json']
   },
   rules: {
-    'prettier/prettier': 'error',
     '@typescript-eslint/no-unused-vars': 'error',
+    'object-curly-newline': [
+      'error',
+      {
+        ObjectExpression: { multiline: true, minProperties: 1 },
+        ObjectPattern: { multiline: true },
+        ImportDeclaration: { multiline: true, minProperties: 3 },
+        ExportDeclaration: { multiline: true, minProperties: 3 }
+      }
+    ]
   },
-  ignorePatterns: [
-    '.idea/*',
-    'README.md',
-    'start.sh',
-    '.github/*',
-    '.eslintrc.js'
-  ]
+  ignorePatterns: ['.idea/*', 'README.md', 'start.sh', '.github/*', '.eslintrc.js']
 }
