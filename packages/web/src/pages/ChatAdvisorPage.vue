ca44cf85232f98114a4c996cead6dfeea1b041bc
Yohann Valentin <pro@yohannvalentin.com>
2023-12-14 15:18:27 +0100

    fix: add iframeResizer on chat with Advisor page

--- packages/web/src/pages/ChatAdvisorPage.vue
+++ packages/web/src/pages/ChatAdvisorPage.vue
@@ -1,26 +1,40 @@
 <template>
-  <div class="fr-container--fluid fr-container--iframe">
+  <div class="fr-container--fluid">
     <iframe
+      v-resize="{ log: true }"
       :src="Contact.iframeSrc"
       class="contact-iframe"
       title="Place des entreprise - france-transition-ecologique"
       sandbox="allow-forms allow-scripts allow-same-origin"
+      width="100%"
     ></iframe>
   </div>
 </template>
 
 <script setup lang="ts">
 import Contact from '@/utils/contact'
-</script>
+import type { Directive, DirectiveBinding } from 'vue'
+import { type IFrameOptions, iframeResizer } from 'iframe-resizer'
+// To make it global see https://github.com/davidjbradshaw/iframe-resizer/blob/master/docs/use_with/vue.md#vue3-with-typescript
 
-<style scoped lang="scss">
-.fr-container--iframe {
-  display: flex;
-  flex-direction: column;
-  height: 100vh;
+interface ResizableHTMLElement extends HTMLElement {
+  iFrameResizer?: {
+    removeListeners: () => void
+  }
 }
-.contact-iframe {
-  flex-grow: 1;
-  overflow: hidden;
+
+const vResize: Directive = {
+  mounted(el: HTMLElement, binding: DirectiveBinding) {
+    const options: IFrameOptions = (binding.value as IFrameOptions) || ({} as IFrameOptions)
+
+    el.addEventListener('load', () => iframeResizer(options, el))
+  },
+  unmounted(el: HTMLElement) {
+    const resizableEl = el as ResizableHTMLElement
+
+    if (resizableEl.iFrameResizer) {
+      resizableEl.iFrameResizer.removeListeners()
+    }
+  }
 }
-</style>
+</script>
