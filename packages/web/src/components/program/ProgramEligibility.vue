b3cab5bacf38a5b122a9620b0f90072d880898f0
Julien Paris <jparis.py@gmail.com>
2023-12-18 18:33:42 +0100

    feat: eligibility on 2 responsive cols w/ order of program props

--- packages/web/src/components/program/ProgramEligibility.vue
+++ packages/web/src/components/program/ProgramEligibility.vue
@@ -7,7 +7,7 @@
     <div class="fr-grid-row fr-grid-row--gutters">
       <div v-for="partIdx in 2" :key="`part-${partIdx}`" class="fr-col-6 fr-col-xs-12">
         <div v-for="(field, idx) in PartsOrder[partIdx - 1]" :key="`elegibility-field-part1-${idx}`" class="fr-mb-8v fr-tee-eligigility">
-          <p class="fr-tee-eligigility-title fr-mb-2v">
+          <p class="fr-tee-eligigility-title fr-mb-1v">
             <span class="fr-mr-1v">{{ eligilityEmojis[field] }}</span>
             {{ field.toString()[0].toUpperCase() + field.toString().slice(1) }}
           </p>
