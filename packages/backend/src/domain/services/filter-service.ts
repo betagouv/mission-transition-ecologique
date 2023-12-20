a167a4c45d2552c8e7270536039e630c1a9a208c
Pierre Camilleri <22995923+pierrecamilleri@users.noreply.github.com>
2023-12-20 12:45:49 +0100

    proper infra current date implementation

--- packages/backend/src/domain/services/filter-service.ts
+++ packages/backend/src/domain/services/filter-service.ts
@@ -1,8 +1,4 @@
+import { currentDateService } from '../../infrastructure/current-date'
 import { createService as createFilterService } from '../filter-programs'
-import { CurrentDateService } from '../spi'
-
-const currentDateService: CurrentDateService = {
-  get: () => '2023-12-20'
-}
 
 export const filterPrograms = createFilterService(currentDateService)
