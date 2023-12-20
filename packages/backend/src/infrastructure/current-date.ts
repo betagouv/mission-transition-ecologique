2bdaadf1df4d59ca887400228484b8b27c4b5ae1
Pierre Camilleri <22995923+pierrecamilleri@users.noreply.github.com>
2023-12-20 16:33:25 +0100

    /!\ REVERT ME
    Make as if current date was "01/01/2024"

--- packages/backend/src/infrastructure/current-date.ts
+++ packages/backend/src/infrastructure/current-date.ts
@@ -1,5 +1,6 @@
 import type { CurrentDateService } from '../domain/program/spi'
 
 export const currentDateService: CurrentDateService = {
-  get: () => new Date().toLocaleDateString('fr-FR')
+  // get: () => new Date().toLocaleDateString('fr-FR')
+  get: () => '01/01/2024'
 }
