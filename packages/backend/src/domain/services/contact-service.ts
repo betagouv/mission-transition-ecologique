betagouv/mission-transition-ecologique

commit c97fae2f492dea903cd29fbb86b153e59fc465c5

feat: filtrage des dispositifs s'il ne sont plus (ou pas encore) valides (#455)
Désolé, la PR est un peu plus costaud qu'elle ne devrait être car j'ai
mélangé du refacto à cette fontionnalité...

# Ajouts fonctionnels

- Les dispositifs sont filtrés selon leur date de début et de fin de
validité. En particulier :
- La règle d'éligibilité des dispositifs est enrichie de conditions
d'applicabilité en fonction des dates de validité (le cas échéant)
- La date du jour est injectée pour l'évaluation sous le nom `date du
jour`
- pour ne pas être dupliquées (données yaml + règle publicodes), les
dates des dispositifs sont injectées pour l'évaluation sous le nom
`dispositif . début de validité` et `dispositif . fin de validité`

La bonne injection des données est couverte par des tests unitaires.

# Refactorings 

- Premier gros refactoring des tests unitaires, avec la création d'une
fonction auxiliaire de test `testHelperPreprocessing` qui, de manière
générique, teste que des données d'entrée (qui peuvent venir de
différentes sources extérieures : questionnaire, dispositif, date du
jour) sont correctement traitées et prises en compte dans le filtrage
avec publicodes.

- Second gros refactoring : j'ai séparé les différents services du
backend qui ont très peu de choses communes (on en avait parlé) dans des
répertoires spécifiques. Chacun hérite de ses types dans "types.ts" et
de ses interfaces dans "spi.ts" pour éviter que tout ça ne soit pas
mélangé.
- toutes les injections de dépendance se font dans le répertoire
"services" (précédemment : directement à l'utilisation dans le
"controller"). Le fichier "api.ts" (en plus d'avoir un nom ambigü avec
l'ubiquité des API REST) a été supprimé, les services à utiliser en
dehors du backend sont clairement identifiés par ce biais-là.
- l'injection se fait via une fonction renommée en `createService`
(précédemment : `makeFeatures`)

# Vérifications fonctionnelles manuelles

packages/backend/src/domain/services/contact-service.ts
@@ -1,0 +1,14 @@
+import { addBrevoContact } from '@tee/backend/src/infrastructure/brevo-API'
+import { createService } from '../contact/contact-features'
+import { ContactInfoRepository } from '../contact/spi'
+
+/**
+ * Defines how to access external data.
+ * Uses the "Repository" pattern, see README.md
+ */
+const brevoRepository: ContactInfoRepository = {
+  add: addBrevoContact
+}
+const service = createService(brevoRepository)
+
+export const postNewContact = service.postNewContact

packages/backend/src/domain/services/establishment-service.ts
@@ -1,0 +1,15 @@
+import { createService } from '../establishment/establishment-features'
+import { getEtablissement } from '@tee/backend/src/infrastructure/sirene-API'
+import { EtablissementRepository } from '../establishment/spi'
+
+/**
+ * Defines how to access external data.
+ * Uses the "Repository" pattern, see README.md
+ */
+const etablissementRepository: EtablissementRepository = {
+  get: getEtablissement
+}
+
+const service = createService(etablissementRepository)
+
+export const fetchEtablissement = service.fetchEtablissement

packages/backend/src/controller/contactsController.ts
@@ -1,17 +1,7 @@
 import { Body, Controller, Post, Route, SuccessResponse, TsoaResponse, Res, Example } from 'tsoa'
-import { createContactFeatures } from '../domain/features'
-import { ContactInfoRepository } from '../domain/spi'
-import { ServiceNotFoundError, ContactInfoBodyAttributes, ContactId } from '../domain/types'
-import { addBrevoContact } from '../infrastructure/brevo-API'
+import { ServiceNotFoundError, ContactInfoBodyAttributes, ContactId } from '../domain/contact/types'
 import { ErrorJSON, ValidateErrorJSON } from './types'
-
-/**
- * Defines how to access external data services.
- * Uses the "Repository" pattern, see README.md
- */
-const brevoRepository: ContactInfoRepository = {
-  add: addBrevoContact
-}
+import { postNewContact } from '../domain/services/contact-service'
 
 interface ServiceNotFoundErrorJSON {
   message: 'Contact not created'
@@ -45,8 +35,7 @@ export class ContactInfoController extends Controller {
     const bodyEmail = requestBody.email
     const bodyAttributes = requestBody.attributes
 
-    const feat = createContactFeatures(brevoRepository)
-    const contactInfoResult = await feat.postNewContact(bodyEmail, bodyAttributes)
+    const contactInfoResult = await postNewContact(bodyEmail, bodyAttributes)
 
     if (contactInfoResult.isErr) {
       const err = contactInfoResult.error

packages/backend/src/controller/etablissementsController.ts
@@ -1,17 +1,7 @@
 import { Body, Controller, Post, Route, SuccessResponse, TsoaResponse, Res, Example } from 'tsoa'
-import { createEtablissementFeatures } from '../domain/features'
-import { EtablissementRepository } from '../domain/spi'
-import { EstablishmentNotFoundError, Etablissement } from '../domain/types'
-import { getEtablissement } from '../infrastructure/sirene-API'
+import { fetchEtablissement } from '../domain/services/establishment-service'
+import { EstablishmentNotFoundError, Etablissement } from '../domain/establishment/types'
 import { ErrorJSON, ValidateErrorJSON } from './types'
-
-/**
- * Defines how to access external data services.
- * Uses the "Repository" pattern, see README.md
- */
-const etablissementRepository: EtablissementRepository = {
-  get: getEtablissement
-}
 
 interface EstablishmentNotFoundErrorJSON {
   message: 'Establishment not found'
@@ -143,8 +133,7 @@ export class SireneController extends Controller {
   ): Promise<Etablissement> {
     const requestedSiret = requestBody.siret
 
-    const feat = createEtablissementFeatures(etablissementRepository)
-    const etablissementResult = await feat.fetchEtablissement(requestedSiret)
+    const etablissementResult = await fetchEtablissement(requestedSiret)
 
     if (etablissementResult.isErr) {
       const err = etablissementResult.error

packages/backend/src/domain/api.ts
@@ -1,8 +1,0 @@
-import { Result } from 'true-myth'
-import { Etablissement, ContactId } from './types'
-
-type EtablissementResult = Result<Etablissement, Error>
-
-export type fetchEtablissement = (siret: string) => Promise<EtablissementResult>
-
-export type postNewContact = (email: string, attributes: object) => Promise<Result<ContactId, Error>>

packages/backend/src/domain/contact/contact-features.ts
@@ -1,0 +1,14 @@
+import type { ContactInfoRepository } from './spi'
+
+/** allows dependency injection */
+export const createService = (repo: ContactInfoRepository) => {
+  /**
+   * postNewContact passes through the Promise of the infrastructure layer
+   * (promise of ContactId in case of success, Error otherwise)
+   */
+  const postNewContact = async (email: string, attributes: object) => {
+    return repo.add(email, attributes)
+  }
+
+  return { postNewContact }
+}

packages/backend/src/domain/contact/spi.ts
@@ -1,0 +1,6 @@
+import { Result } from 'true-myth'
+import { ContactId } from './types'
+
+export type ContactInfoRepository = {
+  add: (email: string, attributes: object) => Promise<Result<ContactId, Error>>
+}

packages/backend/src/domain/contact/types.ts
@@ -1,0 +1,29 @@
+import { CustomError } from '../../helpers/errors'
+
+export interface ContactInfoBodyAttributes {
+  NOM: string
+  PRENOM: string
+  TEL: string
+  SIRET?: string
+  OPT_IN: boolean
+  FORM_NEEDS?: string
+  PROJECT_NEEDS?: string
+  PROJECT_SECTORS?: string[]
+  USER_ROLES?: string
+  USER_GOALS?: string
+  STRUCTURE_SIZE?: string | number
+  PROGRAM_ID?: string
+  ALL_RESPONSES?: string
+}
+
+export interface ContactInfoBody {
+  email: string
+  listIds: number[]
+  attributes: ContactInfoBodyAttributes
+}
+
+export interface ContactId {
+  id: number
+}
+
+export class ServiceNotFoundError extends CustomError {}

commit cecff43f0412f764f04ff83fcf6e3052690c00e8

fix: error with @tee/backend imports. (#464)
Use relative imports instead.

Fixes error introduced in #455

packages/backend/src/domain/services/contact-service.ts
@@ -1,4 +1,4 @@
-import { addBrevoContact } from '@tee/backend/src/infrastructure/brevo-API'
+import { addBrevoContact } from '../../infrastructure/brevo-API'
 import { createService } from '../contact/contact-features'
 import { ContactInfoRepository } from '../contact/spi'
 

packages/backend/src/domain/services/establishment-service.ts
@@ -1,5 +1,5 @@
 import { createService } from '../establishment/establishment-features'
-import { getEtablissement } from '@tee/backend/src/infrastructure/sirene-API'
+import { getEtablissement } from '../../infrastructure/sirene-API'
 import { EtablissementRepository } from '../establishment/spi'
 
 /**