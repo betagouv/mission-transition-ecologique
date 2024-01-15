a30736631057f3a5099164ca6b049e6ecb4bda41
Yohann Valentin <pro@yohannvalentin.com>
2024-01-15 10:26:30 +0100

    feat: add sentry on backend and frontend

--- packages/backend/src/app.ts
+++ packages/backend/src/app.ts
@@ -3,8 +3,31 @@ import { RegisterRoutes } from '../generated/routes'
 import swaggerUi from 'swagger-ui-express'
 import { ValidateError } from 'tsoa'
 import cors from 'cors'
+import * as Sentry from '@sentry/node'
+import { Integrations } from '@sentry/node'
 
 const app: Express = express()
+
+Sentry.init({
+  integrations: [
+    // enable HTTP calls tracing
+    new Sentry.Integrations.Http({ tracing: true }),
+    // enable Express.js middleware tracing
+    new Integrations.Express({ app })
+  ],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 0.5,
+  environment: 'preprod'
+})
+
+// RequestHandler creates a separate execution context using domains, so that every
+// transaction/span/breadcrumb is attached to its own Hub instance
+app.use(Sentry.Handlers.requestHandler())
+// TracingHandler creates a trace for every incoming request
+app.use(Sentry.Handlers.tracingHandler())
 
 app.use(express.json())
 app.use(cors())
@@ -14,7 +37,14 @@ app.use('/api/docs', swaggerUi.serve, async (_req: Request, res: Response) => {
   return res.send(swaggerUi.generateHTML(await import('../generated/swagger.json')))
 })
 
+app.get('/debug-sentry', function mainHandler() {
+  throw new Error('My first Sentry error!')
+})
+
 RegisterRoutes(app)
+
+// The error handler must be before any other error middleware and after all controllers
+app.use(Sentry.Handlers.errorHandler())
 
 app.use(function notFoundHandler(_req, res: Response) {
   res.status(404).send({ message: 'Not Found' })
--- packages/web/src/main.ts
+++ packages/web/src/main.ts
@@ -7,10 +7,35 @@ import VueDsfr from '@gouvminint/vue-dsfr'
 import './assets/main.css'
 import '@gouvfr/dsfr/dist/core/core.main.min.css'
 import { listIcons } from '@/icons'
+import * as Sentry from '@sentry/vue'
 
 const store = createPinia()
 
 const app = createApp(WebApp as Component)
+
+Sentry.init({
+  app,
+  integrations: [
+    new Sentry.BrowserTracing({
+      routingInstrumentation: Sentry.vueRouterInstrumentation(router)
+    }),
+    new Sentry.Replay()
+  ],
+  environment: 'preprod',
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+
+  // Set `tracePropagationTargets` to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ['localhost', /^https:\/\/yourserver\.io\/api/],
+
+  // Capture Replay for 10% of all sessions,
+  // plus for 100% of sessions with an error
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0
+})
 
 app.use(VueDsfr, { icons: listIcons() })
 app.use(store)
