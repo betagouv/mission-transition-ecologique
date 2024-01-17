c86dbb7eb586aad931c19d7ee9d747cfb64aaa7f
Yohann Valentin <pro@yohannvalentin.com>
2024-01-17 11:59:00 +0100

    fix(config): update sentry dsn to readonly on declaration of nodeJS variable

--- packages/backend/src/env.d.ts
+++ packages/backend/src/env.d.ts
@@ -8,7 +8,7 @@ declare global {
       readonly BREVO_SENDER_EMAIL: string
       readonly BPI_FRANCE_CLIENT_ID: string
       readonly BPI_FRANCE_CLIENT_SECRET: string
-      SENTRY_DSN: string | undefined
+      readonly SENTRY_DSN: string | undefined
       SENTRY_ENVIRONMENT: string
       // more env variables...
     }
