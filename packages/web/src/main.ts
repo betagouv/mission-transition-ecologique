dee19472127935fbe1d582df3de0a743d4506a2f
Yohann Valentin <pro@yohannvalentin.com>
2024-01-22 15:00:39 +0100

    fix: clean code

--- packages/web/src/main.ts
+++ packages/web/src/main.ts
@@ -7,14 +7,10 @@ import VueDsfr from '@gouvminint/vue-dsfr'
 import './assets/main.css'
 import '@gouvfr/dsfr/dist/core/core.main.min.css'
 import { listIcons } from '@/icons'
-import MetaEnv from '@/utils/metaEnv'
 
 const store = createPinia()
 
 const app = createApp(WebApp as Component)
-
-console.log(process.env)
-console.log(MetaEnv.deployUrl)
 
 app.use(VueDsfr, { icons: listIcons() })
 app.use(store)
