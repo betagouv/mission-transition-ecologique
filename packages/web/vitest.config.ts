19693e50dd85b1ccb5090d367f2aa56a2f6b5116
Yohann Valentin <pro@yohannvalentin.com>
2024-01-24 14:06:45 +0100

    fix: eslint

--- packages/web/vitest.config.ts
+++ packages/web/vitest.config.ts
@@ -1,10 +1,10 @@
 import { fileURLToPath } from 'node:url'
-import { defineConfig, configDefaults, mergeConfig, type UserConfig } from 'vitest/config'
+import { defineConfig, configDefaults, mergeConfig } from 'vitest/config'
 import viteConfig from './vite.config'
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-call
 export default mergeConfig(
-  viteConfig as UserConfig,
+  viteConfig,
   defineConfig({
     test: {
       environment: 'jsdom',
