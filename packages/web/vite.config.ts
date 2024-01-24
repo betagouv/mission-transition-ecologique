ace5027da1395239dab4ab480c8e4585a4b81127
Yohann Valentin <pro@yohannvalentin.com>
2024-01-24 12:12:41 +0100

    fix: simplify configuration of vite proxy

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -47,9 +47,7 @@ const viteServer: ServerOptions = {
   proxy: {
     '/api': {
       target: 'http://localhost:3000',
-      changeOrigin: true,
-      secure: false,
-      ws: true
+      secure: false
     }
   }
 }
