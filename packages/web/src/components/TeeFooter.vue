e5b55abb330455683dac10f4627d8e7a31d34a88
Julien Paris <jparis.py@gmail.com>
2024-01-31 18:02:26 -0500

    fix: ui fixes on landing

--- packages/web/src/components/TeeFooter.vue
+++ packages/web/src/components/TeeFooter.vue
@@ -210,28 +210,6 @@ const mainPartner = {
 //   img: '/images/logos/aides-territoires.png',
 //   href: 'https://aides-territoires.beta.gouv.fr/'
 // }
-// const partners = [
-//   {
-//     label: 'ADEME',
-//     img: '/images/logos/ademe.svg',
-//     href: 'https://www.ademe.fr/'
-//   },
-//   {
-//     label: 'Bpifrance',
-//     img: '/images/logos/bpi-france.svg',
-//     href: 'https://www.bpifrance.fr/'
-//   },
-//   {
-//     label: 'CCI France',
-//     img: '/images/logos/cci-france.svg',
-//     href: 'https://www.cci.fr/'
-//   },
-//   {
-//     label: 'CMA France',
-//     img: '/images/logos/cma-france.png',
-//     href: 'https://www.artisanat.fr/'
-//   }
-// ]
 
 const mainLinks = [
   // {
