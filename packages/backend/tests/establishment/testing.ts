e4ee3a46ec4508d69996a839e791b3fe2f2cf2b0
Pierre Camilleri <22995923+pierrecamilleri@users.noreply.github.com>
2024-02-05 09:19:00 +0100

    🔵 rename test variable

--- packages/backend/tests/establishment/testing.ts
+++ packages/backend/tests/establishment/testing.ts
@@ -2,7 +2,7 @@ import { Maybe, Result } from 'true-myth'
 import { type EstablishmentRepository } from '../../src/establishment/domain/spi'
 import { type COG2023Mapping } from '../../src/establishment/infrastructure/json/cityToRegionMapping'
 
-const exampleEstablishment = {
+const testEstablishment = {
   siren: '830141321',
   nic: '00034',
   siret: '83014132100034',
@@ -21,7 +21,7 @@ const exampleEstablishment = {
 }
 
 export const dummyEstablishmentRepository: EstablishmentRepository = {
-  get: (_siret: string) => Promise.resolve(Result.ok(exampleEstablishment))
+  get: (_siret: string) => Promise.resolve(Result.ok(testEstablishment))
 }
 
 export const nothingRegionMapping: COG2023Mapping = {
