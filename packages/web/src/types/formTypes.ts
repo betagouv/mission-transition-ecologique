3e2517fd138e0f382a8d1819a6df29a59d6aa0a8
Pierre Camilleri <22995923+pierrecamilleri@users.noreply.github.com>
2024-02-08 14:25:57 +0100

    refactor(establishment): `get /establishments/{siret}` (#558)
    Annule et remplace la PR #96 
    
    > ### Context
    > 
    > The Sirene API is currently served as a "POST /api/insee/get_by_siret"
    endpoint which requires the data to be provided in the request body.
    > ### Suggestion
    > 
    > I would suggest two changes :
    > 
    > - First, the API should be exposed as a GET endpoint (as the name
    suggests). The siret could then be provided as a path parameter instead,
    e.g. "/api/insee/get_by_siret/83014132100034"
    > - I suggest to change the API endpoint name, because of following
    shortcomings with the current name :
    > "insee" should not be part of the name, as the data provider is
    actually none of the business of the frontend. It may change in the
    future (e.g. switching to API entreprise), and a change of routes should
    not be required in this case.
    > "get_by_siret" > if the verb changes to GET, then it should not be
    repeated in the endpoint. Also, REST API good practices is to [name the
    endpoint after the resource collection that is fetched as a noun (see
    2.1)](https://restfulapi.net/resource-naming/), and not with a verb. In
    addition, current naming does not convey what is returned by the API
    call.
    > Therefore, I suggest followin naming : "/api/etablissements/{siret}"
    
    (Seule différence avec ce que j'avais suggéré : `establishments` au lieu
    of `etablissements`).
    
    ### Implémentation
    
    J'ai dû, en sus, implémenter la gestion des paramètres dans l'url (ici
    le siret) pour pouvoir faire appel à ce nouvel endpoint via le front
    malgré le mécanisme de callback générique.
    
    J'ai fait ça via des placeholders : si on fournit une URL de la forme
    `/etablissements/{siret}` ainsi qu'un objet `{siret: "0123"}` à la
    propriété `DataPath` du callback (nouvelle propriété, calquée sur
    `DataBody`), alors `{siret}` est remplacé par `0123` pour l'appel d'API.

--- packages/web/src/types/formTypes.ts
+++ packages/web/src/types/formTypes.ts
@@ -47,6 +47,7 @@ export interface FormCallback {
   headers: HeadersInit
   method: CallbackMethods
   dataBody?: object | object[]
+  dataPath?: Record<string, string>
   dataStructure: object | object[]
   dataMapping: FormCallbackDataMapping[]
   inputCleaning?: Cleaner[] | CleanerReplaceAll[] | CleanerFromJson[] | CleanerFromDict[] | CleanerDefaultIfNull[]
