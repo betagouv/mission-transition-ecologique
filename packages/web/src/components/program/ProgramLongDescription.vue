fe51a31188ab05cab5191d4df2691f330483fdce
Jpy <jparis.py@gmail.com>
2024-02-09 13:10:34 -0500

    accordion component w/ slot for both eligibility and long description in detail page (#567)
    solves #456

--- packages/web/src/components/program/ProgramLongDescription.vue
+++ packages/web/src/components/program/ProgramLongDescription.vue
@@ -1,30 +1,12 @@
 <template>
-  <div class="fr-accordion fr-my-18v">
-    <h3 class="fr-accordion__title">
-      <button
-        class="fr-accordion__btn"
-        :aria-expanded="drawerOpen"
-        :aria-controls="`accordion-${program.id}`"
-        @click="drawerOpen = !drawerOpen"
-      >
-        {{ Translation.t('program.programKnowMore') }}
-      </button>
-    </h3>
-    <div
-      :id="`accordion-${program.id}`"
-      :class="`fr-collapse ${drawerOpen ? 'fr-collapse--expanded' : ''}`"
-      :style="drawerOpen ? '-collapse: -165px; --collapse-max-height: none;' : '-collapse: -203px;'"
+  <div class="fr-mt-2v">
+    <p
+      v-for="(paragraph, idx) in descriptionParagraphs"
+      :key="`long-description-paragraph-${idx}`"
+      :class="`fr-mb-0 fr-pb-${descriptionParagraphs.length - 1 === idx ? '3v' : '0'}`"
     >
-      <div class="fr-pt-8v fr-pb-12v">
-        <p
-          v-for="(paragraph, idx) in descriptionParagraphs"
-          :key="`long-description-paragraph-${idx}`"
-          class="fr-mb-0"
-        >
-          {{ paragraph || '&nbsp;' }}
-        </p>
-      </div>
-    </div>
+      {{ paragraph || '&nbsp;' }}
+    </p>
   </div>
 </template>
 
@@ -32,8 +14,7 @@
 // CONSOLE LOG TEMPLATE
 // console.log(`ProgramLongdescription > FUNCTION_NAME > MSG_OR_VALUE :`)
 
-import { ref, computed, onBeforeMount } from 'vue'
-import Translation from '@/utils/translation'
+import { computed } from 'vue'
 import type { ProgramData } from '@/types'
 
 interface Props {
@@ -41,16 +22,8 @@ interface Props {
 }
 const props = defineProps<Props>()
 
-const drawerOpen = ref<boolean>(false)
-
 const descriptionParagraphs = computed(() => {
   const textRaw = props.program['description longue']
-  return textRaw?.split('\n')
-})
-
-onBeforeMount(() => {
-  if (window.innerWidth > 767) {
-    drawerOpen.value = true
-  }
+  return textRaw?.split('\n') || []
 })
 </script>
