798563cac5ee61a8652090fc124f3e5090fc6049
Julien Paris <jparis.py@gmail.com>
2024-02-20 09:31:21 -0500

    feat: add typeform page + update security headers

---
+++ packages/web/src/pages/TeeAddProgram.vue
@@ -0,0 +1,18 @@
+<template>
+  <!-- MAIN APP COMPONENT  -->
+  <div data-tf-live="01HNJQ5V7EYXZTQAY755QPMA4R"></div>
+</template>
+
+<script setup lang="ts">
+// CONSOLE LOG TEMPLATE
+// console.log(`TeeAddProgram > FUNCTION_NAME > MSG_OR_VALUE :`)
+import { onMounted } from 'vue'
+
+onMounted(() => {
+  const typeformScript = document.createElement('script')
+  typeformScript.async = true
+  typeformScript.setAttribute('type', 'text/javascript')
+  typeformScript.setAttribute('src', 'https://embed.typeform.com/next/embed.js')
+  document.head.appendChild(typeformScript)
+})
+</script>
