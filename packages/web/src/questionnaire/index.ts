9ffd4363747f7e089017a0c6ddf210f8761b3d2e
Pierre Camilleri <22995923+pierrecamilleri@users.noreply.github.com>
2024-02-27 10:12:08 +0100

    🔵 remove unused tracks

--- packages/web/src/questionnaire/index.ts
+++ packages/web/src/questionnaire/index.ts
@@ -12,9 +12,6 @@ import { roles } from './trackRoles'
 
 import { buildingProperty } from './trackBuildingProperty'
 import { buildingSurface } from './trackBuildingSurface'
-import { mobility } from './trackMobility'
-import { mobilityNumberVehicles } from './trackMobilityNumberVehicles'
-import { mobilityEnergy } from './trackMobilityEnergy'
 import { mobilityWishes } from './trackMobilityWishes'
 import { wastesStake } from './trackWastesStake'
 import { wastesSorting } from './trackWastesSorting'
@@ -45,9 +42,6 @@ export const tracks: Track[] = [
 
   buildingProperty,
   buildingSurface,
-  mobility,
-  mobilityNumberVehicles,
-  mobilityEnergy,
   mobilityWishes,
   wastesStake,
   wastesSorting,
