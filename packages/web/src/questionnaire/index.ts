1db7eb72e522e9d507fdb70da257a58514418dc5
Pierre Camilleri <22995923+pierrecamilleri@users.noreply.github.com>
2024-02-28 14:15:42 +0100

    🔵 remove unused tracks

--- packages/web/src/questionnaire/index.ts
+++ packages/web/src/questionnaire/index.ts
@@ -1,14 +1,11 @@
-import { needs } from './trackNeeds'
 import { questionnaireRoute } from './trackQuestionnaireRoute'
 
 import { siret } from './trackStructureSiret'
 import { workforce } from './trackStructureWorkforce'
-// import { sizes } from './trackStructureSizes'
 import { sectors } from './trackStructureSectors'
 import { regions } from './trackStructureRegion'
 
 import { goals } from './trackGoals'
-import { roles } from './trackRoles'
 
 import { buildingProperty } from './trackBuildingProperty'
 import { mobilityWishes } from './trackMobilityWishes'
@@ -16,28 +13,21 @@ import { wastesStake } from './trackWastesStake'
 import { wastesSorting } from './trackWastesSorting'
 import { wastesMaterials } from './trackWastesMaterials'
 import { waterStake } from './trackWaterStake'
-// import { energyTypes } from './trackEnergyTypes'
 import { energyReductionPriority } from './trackEnergyReduction'
 import { strategyAudits } from './trackStrategyAudits'
 import { strategyAuditsSelect } from './trackStrategyAuditsSelect'
-
-// import { status } from './trackStatus'
 
 import { results } from './trackResults'
 import type { Track } from '@/types'
 
 export const tracks: Track[] = [
-  needs,
   questionnaireRoute,
   siret,
-  // sizes,
   workforce,
   sectors,
   regions,
 
-  // status,
   goals,
-  roles,
 
   buildingProperty,
   mobilityWishes,
@@ -46,7 +36,6 @@ export const tracks: Track[] = [
   wastesMaterials,
   waterStake,
   energyReductionPriority,
-  // energyTypes,
   strategyAudits,
   strategyAuditsSelect,
 
