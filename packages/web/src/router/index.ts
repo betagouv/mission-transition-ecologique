d97186c15f3d30d4fb1ce081ac9cd07d99ecb003
ttdm <thibaut.tezenas@gmail.com>
2024-03-27 23:56:30 +0100

    feat: sitemap of static pages, TODO: adding the program pages

--- packages/web/src/router/index.ts
+++ packages/web/src/router/index.ts
@@ -2,7 +2,7 @@
 //console.log(`router.index > FUNCTION_NAME > MSG_OR_VALUE :`)
 
 import { createRouter, createWebHistory, type RouteLocationNormalized, type RouteLocationNormalizedLoaded } from 'vue-router'
-import { routes } from '@/router/route'
+import { routes } from '@/router/routes'
 
 export const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
