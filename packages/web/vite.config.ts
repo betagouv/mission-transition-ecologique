8f55f2db3641404f1dc2fdfca520afea0cdc3401
Yohann Valentin <pro@yohannvalentin.com>
2024-03-28 14:50:06 +0100

    perf: remove optimizeDeps excluded

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -92,9 +92,6 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
       '@public': fileURLToPath(new URL('./public', import.meta.url))
     }
-  },
-  optimizeDeps: {
-    exclude: ['oh-vue-icons/icons']
   }
 })
 
