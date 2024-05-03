6f3e0b6bae7e2e9ff1506a3e5477b36377bfb276
ttdm <thibaut.tezenas@beta.gouv.fr>
2024-05-03 09:31:43 +0200

    chore: cleaning the page stats update (#706)

--- packages/backend/src/statistics/application/statisticsService.ts
+++ packages/backend/src/statistics/application/statisticsService.ts
@@ -2,9 +2,10 @@ import { Result } from 'true-myth'
 import StatisticsFeatures from '../domain/statisticsFeatures'
 import { brevoRepository } from '../../opportunity/infrastructure/api/brevo/brevoDeal'
 import StatsData from '@tee/common/src/stats/types'
+import ProgramService from '../../program/application/programService'
 
 export default class StatisticsService {
   public async get(): Promise<Result<StatsData, Error>> {
-    return await new StatisticsFeatures(brevoRepository).computeStatistics()
+    return await new StatisticsFeatures(brevoRepository, new ProgramService()).computeStatistics()
   }
 }
