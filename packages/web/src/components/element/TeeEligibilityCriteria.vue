d5b59fd0adcdd8273aad27e9b3ed141dcf0d2421
Yohann Valentin <pro@yohannvalentin.com>
2024-05-15 15:45:28 +0200

    feat: add scss to text color, add hidden/unhidden col by breakpoints

--- packages/web/src/components/element/TeeEligibilityCriteria.vue
+++ packages/web/src/components/element/TeeEligibilityCriteria.vue
@@ -3,7 +3,7 @@
     class="fr-grid-row fr-grid-row--center fr-text-center fr-grid-row--middle fr-my-auto"
     :class="bgClass"
   >
-    <div class="fr-col-md-4 fr-col-lg-3 fr-col-xl-2 fr-col-sm-hide">
+    <div class="fr-col-md-4 fr-col-lg-3 fr-col-xl-2 fr-col-hidden-xs fr-col-unhidden-md">
       <div class="fr-col-12">
         <TeeButtonLink
           :to="previousRoute"
@@ -13,40 +13,40 @@
         </TeeButtonLink>
       </div>
     </div>
-    <div class="fr-px-md-2v fr-my-auto fr-col-sm-hide fr-col-md-8 fr-col-lg-9 fr-col-xl-8 fr-text--sm">
+    <div class="fr-px-md-2v fr-my-auto fr-col-hidden-xs fr-col-unhidden-md fr-col-md-8 fr-col-lg-9 fr-col-xl-8 fr-text--sm">
       <div class="fr-container fr-px-md-4w fr-px-0">
         <div class="fr-grid-row">
-          <div class="fr-col-3 fr-bg--blue fr-radius-l-2v fr-px-2v fr-col-content--middle fr-col-justify--center">
+          <div class="fr-col-3 fr-bg--blue--light fr-radius-l-2v fr-px-2v fr-col-content--middle fr-col-justify--center">
             <span
               class="fr-icon-check-line fr-icon--sm fr-mr-1-5v"
               aria-hidden="true"
             />
-            SIRET {{ siretValue }}
+            {{ siretValue }}
           </div>
-          <div class="fr-col-2 fr-bg--blue fr-col-content--middle">
+          <div class="fr-col-2 fr-bg--blue--light fr-col-content--middle">
             <span
               class="fr-icon-check-line fr-icon--sm fr-mr-1-5v"
               aria-hidden="true"
             />
             {{ size }}
           </div>
-          <div class="fr-col-2 fr-bg--blue fr-col-content--middle">
+          <div class="fr-col-2 fr-bg--blue--light fr-col-content--middle">
             <span
               class="fr-icon-check-line fr-icon--sm fr-mr-1-5v"
               aria-hidden="true"
             />
             {{ sector }}
           </div>
-          <div class="fr-col-2 fr-bg--blue fr-col-content--middle">
+          <div class="fr-col-2 fr-bg--blue--light fr-col-content--middle">
             <span
               class="fr-icon-check-line fr-icon--sm fr-mr-1-5v"
               aria-hidden="true"
             />
             {{ localisation }}
           </div>
-          <div class="fr-col-1 fr-bg--blue fr-radius-r-2v fr-col-content--middle fr-text-center">
+          <div class="fr-col-1 fr-bg--blue--light fr-radius-r-2v fr-col-content--middle fr-col-justify--center">
             <span
-              class="fr-icon-pencil-fill fr-icon--sm fr-mr-1-5v"
+              class="fr-icon-pencil-fill fr-icon--sm fr-mr-1-5v fr-text--blue-france"
               aria-hidden="true"
             />
           </div>
@@ -57,6 +57,7 @@
 </template>
 
 <script setup lang="ts">
+import Format from '@/utils/format'
 import TrackStructure from '@/utils/track/trackStructure'
 import type { RouteLocationRaw } from 'vue-router'
 
@@ -68,10 +69,10 @@ interface Props {
 
 const props = defineProps<Props>()
 
-const siretValue = TrackStructure.getSiret()
-const size = TrackStructure.getSizeTitle()
-const sector = TrackStructure.getSector()
-const localisation = TrackStructure.getLocalisation()
+const siretValue = Format.truncate('SIRET ' + TrackStructure.getSiret(), 30)
+const size = Format.truncate(TrackStructure.getSizeTitle(), 30)
+const sector = Format.truncate(TrackStructure.getSector(), 30)
+const localisation = Format.truncate(TrackStructure.getLocalisation(), 30)
 
 const bgClass = computed(() => {
   if (props.bgColor) {
