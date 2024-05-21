773976dbdfd78a3df851729c324d00a4c7b1d401
oumeimaelisbihani <oumeima.el-isbihani@multi.coop>
2024-05-21 15:45:14 +0200

    css

--- packages/web/src/components/questionnaire/TeeQuestionnaireResult.vue
+++ packages/web/src/components/questionnaire/TeeQuestionnaireResult.vue
@@ -16,7 +16,7 @@
       </div>
       <div
         id="tee-app-tracks"
-        class="fr-grid-row--center fr-col fr-col-sm-12 fr-col-md-8 fr-col-lg-9 fr-col-xl-8"
+        class="fr-grid-row--center fr-col fr-col-sm-9 fr-col-md-8 fr-col-lg-9 fr-col-xl-8"
       >
         <Suspense>
           <ProgramList />
