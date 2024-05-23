31f7085297e4949ba7ef6fd41b9afbf82d00adfe
oumeimaelisbihani <oumeima.el-isbihani@multi.coop>
2024-05-23 15:38:36 +0200

    remove padding + remove legend

--- packages/web/src/components/program/list/CatalogResult.vue
+++ packages/web/src/components/program/list/CatalogResult.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="fr-container--fluid fr-container--fluid--no-overflow">
     <div class="fr-grid-row fr-justify-center">
-      <div class="fr-mt-10v fr-mr-5v fr-col-2 fr-col-sm-2 fr-col-md-3 fr-col-lg-2 fr-hidden-xs">
+      <div class="fr-mr-5v fr-col-2 fr-col-sm-2 fr-col-md-3 fr-col-lg-2 fr-hidden-xs">
         <div class="fr-sidemenu fr-sidemenu--sticky">
           <div class="fr-text--bold fr-text-left fr-mb-3v">Filtres</div>
           <ProgramFiltersAccordeon />
