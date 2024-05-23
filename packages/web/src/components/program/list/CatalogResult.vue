2b9f818de6ba84535f67b6f0572e7d4b4099d886
oumeimaelisbihani <oumeima.el-isbihani@multi.coop>
2024-05-23 10:19:23 +0200

    remove h7

--- packages/web/src/components/program/list/CatalogResult.vue
+++ packages/web/src/components/program/list/CatalogResult.vue
@@ -3,7 +3,7 @@
     <div class="fr-grid-row fr-justify-center">
       <div class="fr-mt-10v fr-mr-5v fr-col-2 fr-col-sm-2 fr-col-md-3 fr-col-lg-2 fr-hidden-xs">
         <div class="fr-sidemenu fr-sidemenu--sticky">
-          <div class="fr-h7 fr-text--bold fr-text-left fr-mb-3v">Filtres</div>
+          <div class="fr-text--bold fr-text-left fr-mb-3v">Filtres</div>
           <ProgramFiltersAccordeon />
         </div>
       </div>
