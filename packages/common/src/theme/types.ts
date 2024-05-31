33c36e8a95620abec7f22b5e63ee693ce7603d41
ttdm <thibaut.tezenas@gmail.com>
2024-05-31 09:35:26 +0200

    feat: première proposition format data

--- packages/common/src/theme/types.ts
+++ packages/common/src/theme/types.ts
@@ -1,9 +1,12 @@
 import { PublicodeObjective } from '../questionnaire/types/types'
 
-export interface Theme {
+export interface ObjectiveType {
+  id: string
   title: string
   tagLabel: string
   value: PublicodeObjective
   image: string
   color?: string
+  highlightProjects: number[]
+  displayDots: boolean
 }
