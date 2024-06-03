2de26e6df772b1cf742b1b764bafd072a4283876
dlemoine <contact@dorianlemoine.fr>
2024-06-03 14:40:00 +0200

    refactor: update CSS classes in ProgramForm component

--- packages/web/src/components/program/detail/ProgramForm.vue
+++ packages/web/src/components/program/detail/ProgramForm.vue
@@ -153,7 +153,7 @@
                 :type="opportunityForm.needs.callOut.type"
                 :img="`${publicPath}${opportunityForm.needs.callOut.img}`"
                 :img-container-class="'fr-col-xl-2 fr-hidden fr-unhidden-lg'"
-                :content-class="'fr-pb-2v fr-tee-form-banner'"
+                :content-class="'fr-pb-2v fr-tee-form-banner fr-px-3v fr-px-lg-0'"
               >
                 {{ opportunityForm.needs.callOut.content }}
               </TeeCallout>
