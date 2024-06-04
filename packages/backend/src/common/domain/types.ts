968a8d66564c6bdb8e90b97330464152c24182ac
Yohann Valentin <pro@yohannvalentin.com>
2024-06-04 23:16:42 +0200

    fix: try move objective type on other main directory

--- packages/backend/src/common/domain/types.ts
+++ packages/backend/src/common/domain/types.ts
@@ -1,5 +1,3 @@
 import { ContactId as contactIdFromContactDomain } from '../../opportunity/domain/types'
 import { ContactId as contactIdFromopportunityHubDomain } from '../../opportunityHub/domain/types'
 export type ContactId = contactIdFromContactDomain | contactIdFromopportunityHubDomain
-// export { Objective } from '../../../common/src/questionnaire/types'
-export * from '@tee/common/src/questionnaire/types'
