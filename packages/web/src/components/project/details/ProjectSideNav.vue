0fb5d709a249fd0f2a2d10f78be15ebbd1ac885e
oumeimaelisbihani <oumeima.el-isbihani@multi.coop>
2024-06-18 17:48:18 +0200

    side nav project

--- packages/web/src/components/project/details/ProjectSideNav.vue
+++ packages/web/src/components/project/details/ProjectSideNav.vue
@@ -6,12 +6,14 @@
 </template>
 <script setup lang="ts">
 import { DsfrSideMenuListProps } from '@gouvminint/vue-dsfr/types/components/DsfrSideMenu/DsfrSideMenu.types'
+import { useNavigationStore } from '@/stores/navigation'
 
+const route = useNavigationStore().route
 const menuItems: DsfrSideMenuListProps['menuItems'] = [
-  { id: 'project', to: '#project-details', text: 'Le projet' },
-  { id: 'project-more', to: '#project-more-details', text: 'Pour aller plus loin' },
-  { id: 'aids', to: '#project-aids', text: 'Mes aides' },
-  { id: 'contact', to: '#project-contact', text: 'Contact' },
-  { id: 'linked-project', to: '#project-linked-projects', text: 'Projets complémentaires' }
+  { id: 'project', to: `${route?.fullPath}#project-details`, text: 'Le projet' },
+  { id: 'project-more', to: `${route?.fullPath}#project-more-details`, text: 'Pour aller plus loin' },
+  { id: 'aids', to: `${route?.fullPath}#project-aids`, text: 'Mes aides' },
+  { id: 'contact', to: `${route?.fullPath}#project-contact`, text: 'Contact' },
+  { id: 'linked-project', to: `${route?.fullPath}#project-linked-projects`, text: 'Projets complémentaires' }
 ]
 </script>
