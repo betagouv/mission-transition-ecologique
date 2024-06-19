9b91fcc903008634624aa5e6dda2473d5466f7cd
dlemoine <contact@dorianlemoine.fr>
2024-06-19 16:35:32 +0200

    Revert "fix: refactor StatApi removing dependency on RequestApi"
    This reverts commit bfd7e4fa1583b33edd4094acf0a2f508a528afcb.

--- packages/web/src/service/api/statsApi.ts
+++ packages/web/src/service/api/statsApi.ts
@@ -1,15 +1,6 @@
+import RequestApi from '@/service/api/requestApi'
 import type StatsData from '@tee/common/src/stats/types'
-import { Result } from 'true-myth'
 
-export default class StatApi {
+export default class StatApi extends RequestApi<StatsData> {
   protected readonly url = '/api/statistics'
-
-  async get(): Promise<Result<StatsData, Error>> {
-    try {
-      const response = await fetch(this.url)
-      return Result.ok((await response.json()) as StatsData)
-    } catch (error: unknown) {
-      return Result.err(error as Error)
-    }
-  }
 }
