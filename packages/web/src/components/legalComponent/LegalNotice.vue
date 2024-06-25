26e8bb82781c853f5d32edd30768e113931475e4
Yohann Valentin <pro@yohannvalentin.com>
2024-06-25 11:02:03 +0200

    feat: use composition API with setup syntactic sugar

--- packages/web/src/components/legalComponent/LegalNotice.vue
+++ packages/web/src/components/legalComponent/LegalNotice.vue
@@ -1,57 +1,54 @@
-<script lang="ts">
+<template>
+  <render />
+</template>
+<script setup lang="ts">
+import { defineProps, compile } from 'vue'
 import { LegalNoticeProps } from '@incubateur-ademe/legal-pages-markdown'
-import html from '@incubateur-ademe/legal-pages-markdown/html/LegalNotice_withBeta.html?raw'
-import { PropType } from 'vue'
+import html from '@incubateur-ademe/legal-pages-markdown/html/LegalNotice.html?raw'
 
-export default defineComponent({
-  props: {
-    date: {
-      type: String,
-      required: true
-    },
-    licenceUrl: {
-      type: String,
-      required: true
-    },
-    privacyPolicyUrl: {
-      type: String,
-      required: true
-    },
-    siteUrl: {
-      type: String,
-      required: true
-    },
-    siteName: {
-      type: String,
-      required: true
-    },
-    thirdParties: {
-      type: Array as PropType<LegalNoticeProps['thirdParties']>,
-      required: false,
-      default: () => []
-    },
-    siteHost: {
-      type: Object as PropType<LegalNoticeProps['siteHost']>,
-      required: false,
-      default: () => ({})
-    }
+const props = defineProps({
+  date: {
+    type: String,
+    required: true
   },
-  setup(props) {
-    let element_thirdParties = ''
-    if (props.thirdParties) {
-      for (const { name, url, text } of props.thirdParties) {
-        element_thirdParties += `
-        <p>
-          ${text} <a href="${url}" target="_blank" rel="noopener noreferrer">${name}</a>
-        </p>
-      `
-      }
-    }
-    return {
-      props,
-      element_thirdParties
-    }
+  licenceUrl: {
+    type: String,
+    required: true
   },
-  template: html
+  privacyPolicyUrl: {
+    type: String,
+    required: true
+  },
+  siteUrl: {
+    type: String,
+    required: true
+  },
+  siteName: {
+    type: String,
+    required: true
+  },
+  thirdParties: {
+    type: Array as PropType<LegalNoticeProps['thirdParties']>,
+    required: false,
+    default: () => []
+  },
+  siteHost: {
+    type: Object as PropType<LegalNoticeProps['siteHost']>,
+    required: false,
+    default: () => ({})
+  }
 })
+
+let element_thirdParties = ''
+if (props.thirdParties) {
+  for (const { name, url, text } of props.thirdParties) {
+    element_thirdParties += `
+    <p>
+      ${text} <a href="${url}" target="_blank" rel="noopener noreferrer">${name}</a>
+    </p>
+  `
+  }
+}
+
+const render = h({ render: compile(html.replace('{{element_thirdParties}}', element_thirdParties)), setup: () => props })
 </script>
