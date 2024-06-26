8a48dacf1d33830a4c3d19a417b9c613472dfb18
dlemoine <contact@dorianlemoine.fr>
2024-06-26 14:20:09 +0200

    refactor: add conditional rendering to ResultHeader

--- packages/web/src/components/questionnaire/result/list/ResultHeader.vue
+++ packages/web/src/components/questionnaire/result/list/ResultHeader.vue
@@ -3,7 +3,10 @@
     <div class="fr-col-12">
       <h1 class="fr-mb-4v fr-text--blue-france">Vos résultats</h1>
     </div>
-    <div class="fr-hidden fr-unhidden-md fr-mb-2v fr-col-12 fr-px-2v fr-px-md-0 fr-text--blue-france">
+    <div
+      v-if="UsedTrack.isSpecificGoal()"
+      class="fr-hidden fr-unhidden-md fr-mb-2v fr-col-12 fr-px-2v fr-px-md-0 fr-text--blue-france"
+    >
       <p v-html="resume"></p>
     </div>
   </div>
@@ -12,6 +15,7 @@
 <script setup lang="ts">
 import TrackStructure from '@/utils/track/trackStructure'
 import Translation from '@/utils/translation'
+import UsedTrack from '@/utils/track/usedTrack'
 
 const resume: string = Translation.t('programResults.resume', {
   effectif: Translation.t('enterprise.structureSize.' + TrackStructure.getSize()),
