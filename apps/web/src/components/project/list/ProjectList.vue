db7bd4eff627edc0e501f2dba54e7c3e2d5e53cc
dlemoine <contact@dorianlemoine.fr>
2024-07-12 14:31:15 +0200

    fix: align project list to the left

--- apps/web/src/components/project/list/ProjectList.vue
+++ apps/web/src/components/project/list/ProjectList.vue
@@ -59,7 +59,7 @@
   >
     <div class="fr-container fr-m-0 fr-p-0 fr-mt-2w fr-hidden fr-unhidden-lg">
       <div class="fr-col-12 fr-col-md-10 fr-col-offset-md-2">
-        <div class="fr-grid-row fr-grid-row--gutters fr-grid-row--center fr-px-2w fr-px-md-2v">
+        <div class="fr-grid-row fr-grid-row--gutters fr-grid-row--left fr-px-2w fr-px-md-2v">
           <router-link
             v-for="project in nonPriorityProjects"
             :id="project.slug"
