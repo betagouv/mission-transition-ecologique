1b5bbd7d7691a954481e4bee9cd068fe9ba97c1c
oumeimaelisbihani <oumeima.el-isbihani@multi.coop>
2024-07-16 11:55:18 +0200

    remove top bar on result

--- apps/web/src/components/questionnaire/TeeQuestionnaireResult.vue
+++ apps/web/src/components/questionnaire/TeeQuestionnaireResult.vue
@@ -3,30 +3,10 @@
     :id="RouteName.QuestionnaireResult"
     class="fr-container--fluid fr-container--fluid--no-overflow"
   >
-    <TeeEligibilityCriteriaBar
-      v-if="linkToPreviousButton"
-      :bg-color="Color.blueLight"
-      :bg-bar-color="Color.blueLighted"
-      :previous-route="linkToPreviousButton"
-    />
     <ResultList />
   </div>
 </template>
 
 <script setup lang="ts">
-import { Color } from '@/types'
 import { RouteName } from '@/types/routeType'
-import { useUsedTrackStore } from '@/stores/usedTrack'
-import { useNavigationStore } from '@/stores/navigation'
-
-const navigationStore = useNavigationStore()
-const usedTrackStore = useUsedTrackStore()
-
-const linkToPreviousButton = computed(() => {
-  const trackId = usedTrackStore.getPreviousCompletedUsedTrackId()
-  if (trackId) {
-    return navigationStore.routeByTrackId(trackId)
-  }
-  return null
-})
 </script>
