45fd907901711669815d83c5b4df00843a46c034
Yohann Valentin <pro@yohannvalentin.com>
2024-07-19 12:31:40 +0200

    feat: improvement of monitoring class

--- libs/backend-ddd/src/common/domain/monitoring/monitor.ts
+++ libs/backend-ddd/src/common/domain/monitoring/monitor.ts
@@ -2,8 +2,10 @@ import * as Sentry from '@sentry/node'
 import Config from '../../../config'
 
 export default class Monitor {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  static error(message: string, data?: any) {
+  private static _debug: boolean = Config.DEBUG as unknown as boolean
+  private static _message?: string
+
+  static error(message: string, data?: { [key: string]: unknown }) {
     Sentry.captureMessage(message, (scope) =>
       scope.addBreadcrumb({
         type: 'error',
@@ -12,10 +14,14 @@ export default class Monitor {
         data: data
       })
     )
+
+    this._message = message
+    this.log()
+
+    return this
   }
 
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  static warning(message: string, data?: any) {
+  static warning(message: string, data?: { [key: string]: unknown }) {
     Sentry.captureMessage(message, (scope) =>
       scope.addBreadcrumb({
         type: 'warning',
@@ -24,11 +30,32 @@ export default class Monitor {
         data: data
       })
     )
+
+    this._message = message
+    this.log()
+
+    return this
   }
 
-  static debug(message: string) {
-    if (Config.DEBUG) {
-      console.log('debugging log : ', message)
+  static withDebug() {
+    this._debug = true
+    this.log()
+
+    return this
+  }
+
+  static log(message: string | undefined = undefined, force = false) {
+    if (this._debug || force) {
+      if (message) {
+        this._message = message
+      }
+      if (this._message) {
+        console.log('debugging log : ', this._message)
+        this._message = undefined
+        this._debug = Config.DEBUG as unknown as boolean
+      }
     }
+
+    return this
   }
 }
