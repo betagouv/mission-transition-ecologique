721987f38de097dc24b9bffd919807a277bc922e
ttdm <thibaut.tezenas@beta.gouv.fr>
2024-07-24 16:07:12 +0200

    feat: capture sentry errors in the backend (#866)
    Ajout de points de transmission d'erreurs du backend à sentry. 
    
    Transmissions d'erreur au niveau des controleurs pour capturer la totalité des erreurs et à très bas niveau pour être capable d'identifier précisément des opérations qui peuvent être problématiques. 
    
    close #679
    
    ---------
    
    Co-authored-by: Yohann Valentin <pro@yohannvalentin.com>

--- libs/backend-ddd/src/common/index.ts
+++ libs/backend-ddd/src/common/index.ts
@@ -1,3 +1,4 @@
 export * from './controller/jsonError'
 export { default as ServiceNotFoundError } from './domain/api/serviceNotFoundError'
 export { ensureError } from './domain/error/errors'
+export { default as Monitor } from './domain/monitoring/monitor'
