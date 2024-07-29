ccc0cf002913c9e678166a24dc48a15d0e0cb595
ttdm <thibaut.tezenas@gmail.com>
2024-07-29 16:40:54 +0200

    feat: delete all unused dataProgram fields

--- libs/data/src/program/types/domain.ts
+++ libs/data/src/program/types/domain.ts
@@ -3,7 +3,6 @@ import { Theme } from '../../theme/types/domain'
 export interface DataProgram {
   id: number
   'Id fiche dispositif': string
-  'Identifiant fonctionnel': string
   Titre: string
   Promesse: string
   'Description courte': string
@@ -12,15 +11,8 @@ export interface DataProgram {
   'Dispositif activable en autonomie': boolean
   'Parcours "Je ne sais pas par où commencer"': boolean
   'URL externe': string
-  'Coût reste à charge': string
-  'Coût global': string
   "Montant de l'aide": string
-  'Aides financières': string
-  'MontantMin aide': string
-  'MontantMax aide': string
   "Durée de l'aide": string
-  "Durée estimé pour l'entreprise": string
-  'Date de cloture intermédiaire': string
   DISPOSITIF_DATE_DEBUT: string
   DISPOSITIF_DATE_FIN: string
   'Zones Spécifiques (géographie)': string
@@ -31,27 +23,7 @@ export interface DataProgram {
   'Eligibilité Existence': string
   'Eligibilité Naf': string
   'Eligibilité Sectorielle': string
-  'Thèmes ADEME': string
-  'Sous-thèmes ADEME': string
-  'Type de projet ADEME': string
   Propriétaire: string
-  Surface: string
-  'Mode trajet domicile-travail': string
-  'Véhicule motorisé': string
-  'TypeEssence, Elec...': string
-  Dechet: string
-  Tri: string
-  Matières: string
-  'Enjeu Eau': string
-  Energie: string
-  AuditEnvironnemental: string
-  'Bilan GES /Carbon': string
-  'Audit de performance énergétique des bâtiments ou Certification ISO 50001': string
-  'Audit conso énergie': string
-  'Audit Eau': string
-  'Audit Dechet': string
-  'Audit matières premières': string
-  'Certification ISO 14001': string
   AAgriculture: string
   'BIndustries extractives': string
   CIndustrie: string
@@ -73,39 +45,6 @@ export interface DataProgram {
   SAutres: string
   TActivités: string
   UActivités: string
-  '<5': string
-  Entre5et20: string
-  Entre20et50: string
-  Entre50et250: string
-  Plus250: string
-  Stats: string
-  'Indice Présence': string
-  'Rénover son bâtiment': string
-  "Economiser l'énergie": string
-  "Economiser  l'eau": string
-  'Réaliser un investissement pour décarbonner son matériel de production': string
-  'Trier, réduire, recycler ses déchets ses déchets': string
-  'Réaliser un diagnostic environnemental': string
-  'Former ses collaborateurs': string
-  'Eco-concevoir son produit': string
-  'Décarboner son activité': string
-  'Recruter pour réaliser sa transition': string
-  'Titre historique au 01 Septembre 2023': string
-  FRANCE_2030: string
-  AAP_GROUPEMENT_TECHNIQUE_OPALE: string
-  AAP_GROUPEMENT_COMPLEXITE: string
-  DISPOSITIF_DATE_DERNIERE_MODIF: string
-  AAP_ACRONYME: string
-  DATE_MISE_A_JOUR_DONNEES: string
-  'Lien supplémentaire': string
-  'Type de dispositif': string
-  'Année du dispositif': string
-  Cibles: string
-  EnergieConso: string
-  'Date de création': string
-  'Date de validation': string
-  'Date de publication': string
-  'Opérateur MAJ': string
   'Autres opérateurs': Operator[]
   'Couverture géographique': GeographicCoverage
   'Zones géographiques': GeographicAreas[]
