1a2c9d2ad53fa267270fbafef3d4cf506f920b06
dlemoine <contact@dorianlemoine.fr>
2024-08-01 11:12:04 +0200

    fix: typo in ContactHelp component

--- apps/web/src/components/contact/ContactHelp.vue
+++ apps/web/src/components/contact/ContactHelp.vue
@@ -5,7 +5,7 @@
         <div class="fr-col-12">
           <div class="fr-follow__newsletter">
             <div>
-              <h5 class="fr-h5 fr-follow__title">Vous souhaitez échangez avec un conseiller ?</h5>
+              <h5 class="fr-h5 fr-follow__title">Vous souhaitez échanger avec un conseiller ?</h5>
               <p class="fr-text--sm fr-follow__desc">
                 Vous ne trouvez pas ce que vous recherchez ? écrivez-nous, nous vous mettrons en relation avec le bon interlocuteur pour
                 répondre à votre question.
