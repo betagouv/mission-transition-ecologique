3e5441c7b3eabd7b0262edad60a850db588403f0
ttdm <thibaut.tezenas@gmail.com>
2024-08-19 18:03:07 +0200

    creating a test that works on all program result pages. Commenting some browser not installed on my machine

---
+++ apps/web-e2e/programResults.spec.ts
@@ -0,0 +1,42 @@
+import { test, expect } from '@playwright/test'
+
+const productionUrl = 'https://mission-transition-ecologique.beta.gouv.fr/'
+const testUrl = 'http://localhost:4242/'
+
+const queryUrls = [
+  'aides-entreprise',
+  'questionnaire/resultat?choix-du-parcours=je-ne-sais-pas-par-ou-commencer&siret=83014132100034&effectif=TPE&locaux=proprietaire&mobilite=oui&matieres-premieres=oui&tri-dechets=non&dechets=oui&gestion-eau=oui&energie=non&audit=non#questionnaire-resultat',
+  'questionnaire/resultat/projet/isolation-thermique?choix-du-parcours=j-ai-un-projet&siret=83014132100034&effectif=TPE&objectifs=r%C3%A9nover+mon+b%C3%A2timent',
+  'questionnaire/resultat/projet/sensibilisation-equipes?choix-du-parcours=je-ne-sais-pas-par-ou-commencer&siret=83014132100034&effectif=TPE&locaux=proprietaire&mobilite=oui&matieres-premieres=oui&tri-dechets=non&dechets=oui&gestion-eau=oui&energie=non&audit=non'
+]
+
+queryUrls.forEach((queryUrl, id) => {
+  test(`Verify content and elements for query number ${id}`, async ({ page }) => {
+    const fullProductionUrl = `${productionUrl}${queryUrl}`
+    const fullLocalUrl = `${testUrl}${queryUrl}`
+
+    await page.goto(fullProductionUrl)
+    try {
+      await page.waitForSelector('div.fr-card__start.fr-mb-2v', { timeout: 3000 })
+    } catch (error) {
+      // expected when no elements are found
+      // it also happen in some other tests cases where there are elements for an unknown reason
+    }
+    const elementsProduction = await page.$$eval('div.fr-card__start.fr-mb-2v > p', (els) => els.map((el) => el.innerHTML.trim()))
+
+    await page.goto(fullLocalUrl)
+    try {
+      await page.waitForSelector('div.fr-card__start.fr-mb-2v', { timeout: 3000 })
+    } catch (error) {
+      // expected when no elements are found
+      // it also happen in some other tests cases where there are elements for an unknown reason
+    }
+    const elementsLocal = await page.$$eval('div.fr-card__start.fr-mb-2v > p', (els) => els.map((el) => el.innerHTML.trim()))
+    console.log(elementsProduction.length)
+
+    expect(elementsLocal.length).toBe(elementsProduction.length)
+    for (let i = 0; i < elementsProduction.length; i++) {
+      expect(elementsProduction[i]).toBe(elementsLocal[i])
+    }
+  })
+})
--- playwright.config.ts
+++ playwright.config.ts
@@ -33,36 +33,36 @@ export default defineConfig({
       use: { ...devices['Desktop Firefox'] }
     },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] }
-    },
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] }
+    // },
 
     /* Test against mobile viewports. */
     {
       name: 'Mobile Chrome',
       use: { ...devices['Pixel 5'] }
-    },
-    {
-      name: 'Mobile Safari',
-      use: { ...devices['iPhone 12'] }
-    },
+    }
+    // {
+    //   name: 'Mobile Safari',
+    //   use: { ...devices['iPhone 12'] }
+    // },
 
     /* Test against branded browsers. */
-    {
-      name: 'Microsoft Edge',
-      use: { ...devices['Desktop Edge'], channel: 'msedge' }
-    },
-    {
-      name: 'Google Chrome',
-      use: { ...devices['Desktop Chrome'], channel: 'chrome' }
-    }
+    // {
+    //   name: 'Microsoft Edge',
+    //   use: { ...devices['Desktop Edge'], channel: 'msedge' }
+    // },
+    // {
+    //   name: 'Google Chrome',
+    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' }
+    // }
   ],
 
   /* Run your local dev server before starting the tests */
   webServer: {
     command: 'npm run dev:build:start',
-    url: 'http://127.0.0.1:4242',
+    url: 'http://localhost:4242',
     reuseExistingServer: !process.env.CI
   }
 })
