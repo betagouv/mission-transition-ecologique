8ac790127dc42224324822299018af292f131e4a
Yohann Valentin <pro@yohannvalentin.com>
2024-09-04 14:41:30 +0200

    fix: add import missing

--- apps/web/src/pages/TeePersonalDataPage.vue
+++ apps/web/src/pages/TeePersonalDataPage.vue
@@ -16,6 +16,7 @@
 // CONSOLE LOG TEMPLATE
 // console.log(`TeePersonalDataPage > FUNCTION_NAME > MSG_OR_VALUE :`)
 
+import { RouteName } from '@/types'
 import { PrivacyPolicyPropsCookie, PrivacyPolicyPropsThirdParty, PrivacyPolicy } from '@incubateur-ademe/legal-pages-vue3'
 
 const thirdParties: PrivacyPolicyPropsThirdParty[] = [
