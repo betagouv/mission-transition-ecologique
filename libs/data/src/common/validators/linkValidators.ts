e6dd2f74eb8042af7507de1a11cf76189238c5a7
ttdm <thibaut.tezenas@gmail.com>
2024-09-28 18:01:51 +0200

    Use playwright to get better link tests

--- libs/data/src/common/validators/linkValidators.ts
+++ libs/data/src/common/validators/linkValidators.ts
@@ -2,7 +2,7 @@
 import axios from 'axios'
 import fetch from 'node-fetch'
 import https from 'https'
-// import { chromium } from 'playwright'
+import { chromium } from 'playwright'
 
 export class LinkValidator {
   public static async isValidLink(link: string) {
@@ -12,10 +12,9 @@ export class LinkValidator {
       result = await this._axiosValidation(link)
       await new Promise((resolve) => setTimeout(resolve, 50))
     }
-
-    // if (!result) {
-    // result = await this._playwrightValidation(link)
-    // }
+    if (!result && this._disablePlayWright()) {
+      result = await this._playwrightValidation(link)
+    }
     return result
   }
 
@@ -51,42 +50,46 @@ export class LinkValidator {
     return false
   }
 
-  // private static async _playwrightValidation(link: string): Promise<boolean> {
-  //   const browser = await chromium.launch()
-  //   const context = await browser.newContext({
-  //     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-  //     extraHTTPHeaders: {
-  //       Referer: 'https://mission-transition-ecologique.beta.gouv.fr/',
-  //       Accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-  //       'Accept-Language': 'en-US,en;q=0.5',
-  //       Connection: 'keep-alive'
-  //     }
-  //   })
-  //   const page = await context.newPage()
+  private static async _playwrightValidation(link: string): Promise<boolean> {
+    const browser = await chromium.launch()
+    const context = await browser.newContext({
+      userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+      extraHTTPHeaders: {
+        Referer: 'https://mission-transition-ecologique.beta.gouv.fr/',
+        Accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        'Accept-Language': 'en-US,en;q=0.5',
+        Connection: 'keep-alive'
+      }
+    })
+    const page = await context.newPage()
 
-  //   const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms))
-  //   try {
-  //     const response = await page.goto(link)
-  //     for (let i = 0; i < 10; i++) {
-  //       await page.evaluate(() => window.scrollBy(0, Math.random() * 100))
-  //       const x = Math.random() * 500 + 300
-  //       const y = Math.random() * 500 + 100
-  //       await page.mouse.move(x, y, { steps: 50 })
-  //       await delay(Math.random() * 500)
-  //     }
-  //     // await delay(5000)
-  //     console.log('waitint number2 ')
-  //     if (response && response.status() < 400) {
-  //       return true
-  //     } else {
-  //       console.error(`Playwright failed to access ${link}`, response?.status())
-  //       return false
-  //     }
-  //   } catch (error) {
-  //     console.error(`Playwright Error accessing ${link}:`, error)
-  //     return false
-  //   } finally {
-  //     await browser.close()
-  //   }
-  // }
+    const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms))
+    try {
+      const response = await page.goto(link)
+      // simulate some actions to bypass the bpi protection
+      for (let i = 0; i < 10; i++) {
+        await page.evaluate(() => window.scrollBy(0, Math.random() * 100))
+        const x = Math.random() * 500 + 300
+        const y = Math.random() * 500 + 100
+        await page.mouse.move(x, y, { steps: 50 })
+        await delay(Math.random() * 500)
+      }
+      if (response && response.status() < 400) {
+        return true
+      } else {
+        return false
+      }
+    } catch (error) {
+      return false
+    } finally {
+      await browser.close()
+    }
+  }
+
+  private static _disablePlayWright(): boolean {
+    if (process.env['LINK_VALIDATOR_PLAYWRIGHT_DISABLE']) {
+      return process.env['LINK_VALIDATOR_PLAYWRIGHT_DISABLE'] !== 'true'
+    }
+    return true
+  }
 }
