c30b4ac1c6136de7653413c89af698754952a8d6
dolemoine <43757076+dolemoine@users.noreply.github.com>
2024-10-25 14:11:33 +0200

    refactor: Refactor eligibility bar component (#1211)
    ###Résumé :
    
    Cette pull request introduit plusieurs modifications pour améliorer la
    fonctionnalité d'éligibilité dans les composants de détail du programme.
    Les mises à jour principales incluent l'ajout d'un nouveau composant
    `ProgramEligibilityBar`, la suppression de l'ancien composant
    `TeeEligibilityCriteriaBar`, et des mises à jour des types de données du
    programme pour inclure les informations d'éligibilité.
    
    ### Améliorations de la fonctionnalité d'éligibilité :
    
    * **Nouveau composant de barre d'éligibilité :**
    - Ajout du composant `ProgramEligibilityBar` pour afficher les messages
    et les liens d'éligibilité en fonction du statut d'éligibilité du
    programme.
    (`apps/web/src/components/program/detail/ProgramEligibilityBar.vue`,
    [apps/web/src/components/program/detail/ProgramEligibilityBar.vueR1-R65](diffhunk://#diff-e2c76203315a21f013245296588564f5dd3ca2a2575112b4708fb2750bb3dec6R1-R65))
    - Intégration de `ProgramEligibilityBar` dans `ProgramHeader.vue`.
    (`apps/web/src/components/program/detail/ProgramHeader.vue`,
    [apps/web/src/components/program/detail/ProgramHeader.vueR2-L9](diffhunk://#diff-5ab97ca0adea050e0f9a249ecedcb104719a24c132f57719403b358308e46a99R2-L9))
    
    * **Suppression de l'ancienne barre de critères d'éligibilité :**
    - Suppression du composant `TeeEligibilityCriteriaBar` et de ses
    références dans divers fichiers.
    (`apps/web/src/components/program/eligibilityCriteria/TeeEligibilityCriteriaBar.vue`,
    [[1]](diffhunk://#diff-3ad051b56a3106db1a8bb456dde6af4b60cc68d0f1753de9c5629a5067a55475L1-L87);
    `apps/web/src/components/project/details/ProjectHeader.vue`,
    [[2]](diffhunk://#diff-f0f99f54b562996255e6fc682b87e4e8e3dc9f5fed1750ed623a18f6d6c3cd55L3-L7)
    
    ### Mises à jour des types de données du programme :
    
    * **Mise à jour du type de données du programme :**
    - Ajout du champ `eligibility` à l'interface `ProgramData`.
    (`apps/web/src/types/program/programTypes.ts`,
    [apps/web/src/types/program/programTypes.tsR35](diffhunk://#diff-a155dc6f14ec29ad6fcf90914556d5e0cefccad6f3893a78f0d8447b748f55d0R35))
    - Introduction de l'énumération `ProgramEligibilityType` pour
    représenter les différents statuts d'éligibilité.
    (`libs/data/src/program/types/shared.ts`,
    [libs/data/src/program/types/shared.tsR22-R27](diffhunk://#diff-24f08ff5d4a9ff94766bd03425342506be91e27293e86d81bd1cd19dd89e3420R22-R27))
    
    ### Modifications diverses :
    
    * **Groupe d'accordéon pour les détails du programme :**
    - Enveloppement des détails du programme dans un groupe d'accordéon pour
    une meilleure organisation.
    (`apps/web/src/components/program/detail/ProgramDetail.vue`,
    [[1]](diffhunk://#diff-d392d63033eccce730c3f9948f149ea8a5670fa8dc7b28d18f8f5e8bc23a0161R190)
    [[2]](diffhunk://#diff-d392d63033eccce730c3f9948f149ea8a5670fa8dc7b28d18f8f5e8bc23a0161R214)
    
    * **Mise à jour de l'utilitaire de défilement :**
    - Mise à jour de l'utilitaire de défilement pour référencer le nouvel ID
    de la barre d'éligibilité. (`apps/web/src/utils/scroll.ts`,
    [apps/web/src/utils/scroll.tsL5-R5](diffhunk://#diff-7bdafdabfbd383ac0c914f4d790fd1e80c3f431c810a6e9d211556ed78d955ceL5-R5))
    
    Ces changements améliorent collectivement l'expérience utilisateur en
    fournissant des informations d'éligibilité claires et dynamiques
    directement dans les détails du programme.
    
    ### App test :
    
    [https://tee-preprod-pr1211.osc-fr1.scalingo.io/](https://tee-preprod-pr1211.osc-fr1.scalingo.io/)
    
    ### Linked issue : 
    close #1103

---
+++ apps/web/src/components/program/detail/ProgramEligibilityBar.vue
@@ -0,0 +1,68 @@
+<template>
+  <TeeEligibilityBar
+    :bg-color="getEligibilityColor"
+    :message="getEligibilityMessage"
+    :link="getEligibilityLink"
+  />
+</template>
+<script setup lang="ts">
+import { Color, type ProgramData as ProgramType, ProgramEligibilityType, RouteName } from '@/types'
+import { TeeEligibilityBarLink, TeeEligibilityBarMessage } from '@/components/program/eligibility/TeeEligibilityBar.vue'
+import { useProgramStore } from '@/stores/program'
+import { ref } from 'vue'
+
+const program = ref<ProgramType>()
+
+const getEligibilityMessage: ComputedRef<TeeEligibilityBarMessage> = computed(() => {
+  switch (program.value?.eligibility) {
+    case ProgramEligibilityType.Eligible:
+      return {
+        default: 'Votre entreprise remplit les critères pour bénéficier de cette aide.',
+        mobile: 'Vous entreprise peut prétendre à cette aide.',
+        icon: 'fr-icon-checkbox-circle-fill'
+      }
+    case ProgramEligibilityType.PartiallyEligible:
+      return {
+        default: 'Votre entreprise semble éligible à cette aide.',
+        mobile: 'Votre pouvez être éligible.',
+        icon: 'fr-icon-checkbox-circle-fill'
+      }
+    case ProgramEligibilityType.NotEligible:
+    default:
+      return {
+        default: "Oups, votre entreprise n'est pas éligible à cette aide.",
+        mobile: "Vous n'êtes pas éligible",
+        icon: 'fr-icon-close-circle-fill'
+      }
+  }
+})
+
+const getEligibilityColor: ComputedRef<Color> = computed(() => {
+  return program.value?.eligibility === ProgramEligibilityType.NotEligible ? Color.red : Color.greenLightnessed
+})
+
+const getEligibilityLink: ComputedRef<TeeEligibilityBarLink | undefined> = computed(() => {
+  switch (program.value?.eligibility) {
+    case ProgramEligibilityType.PartiallyEligible:
+      return {
+        hash: 'program-details-accordion-group',
+        label: 'Voir les autres critères à respecter',
+        labelMobile: 'Vérifier les critères'
+      }
+    case ProgramEligibilityType.NotEligible:
+      return {
+        url: RouteName.CatalogPrograms,
+        label: 'Voir les aides pour mon entreprise',
+        labelMobile: 'Voir les aides éligibles',
+        isButtonLink: true
+      }
+    case ProgramEligibilityType.Eligible:
+    default:
+      return undefined
+  }
+})
+
+onBeforeMount(() => {
+  program.value = useProgramStore().currentProgram
+})
+</script>
