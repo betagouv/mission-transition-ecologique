8aaf4dcccd830662b5da6d490e826b6717f610fa
oumeimaelisbihani <oumeima.el-isbihani@multi.coop>
2024-10-28 16:50:26 +0100

    transition

--- apps/web/src/components/element/form/TeeFormElementCheckbox.vue
+++ apps/web/src/components/element/form/TeeFormElementCheckbox.vue
@@ -28,6 +28,8 @@ import { BooleanFieldInputType } from '@/types'
 interface Props {
   field: BooleanFieldInputType
   publicPath: string
+  getErrorMessage: () => ''
+  getValidMessage: () => ''
 }
 
 defineProps<Props>()
