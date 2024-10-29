e971f75dcc98ac8ea8bf14e095b625213ea59785
Oumeima El Isbihani <31712244+oumeimaelisbihani@users.noreply.github.com>
2024-10-29 17:46:04 +0100

    Feat: CTA "vous êtes ? " dans le header (#1219)
    - bouton dans le header (stylisé selon la taille de l'écran) qui pour
    l'instant change juste l'état d'affichage (deuxieme étape : la modale)
    - suppression des topbar dans le project detail et program detail
    Pour tester : https://tee-preprod-pr1219.osc-fr1.scalingo.io/ 
    Info : pour l'instant c'est des fausses données, le bouton est bien
    validé par Coline
    
    Remarques : 
    J'ai bien mis du temps sur le badge du bouton et j'en suis pas
    complètement satisfaite (beaucoup de css) du coup si vous voyez des
    pistes d'amélioration pour que ce soit plus réutilisable je suis
    preneuse

---
+++ apps/web/src/components/identification/RegisterCTA.vue
@@ -0,0 +1,82 @@
+<template>
+  <TeeDsfrButton
+    :class="registrationStatus ? 'fr-btn--tertiary-no-outline' : ''"
+    @click="emit('click')"
+  >
+    <span
+      v-if="registrationStatus || Breakpoint.isSmallScreen()"
+      :class="Breakpoint.isSmallScreen() ? 'fr-icon--lg' : 'fr-pr-2w'"
+      class="fr-icon-account-circle-fill register-icon-profile"
+    >
+      <span
+        :id="Breakpoint.isSmallScreen() ? 'badge-mobile' : 'base-badge'"
+        :class="badgeIcon"
+        class="fr-text--blue-france fr-radius-a--2v register-badge"
+      >
+      </span>
+    </span>
+
+    <span
+      v-if="!Breakpoint.isSmallScreen()"
+      id="register-text"
+      :class="registrationStatus ? '' : 'fr-text--yellow'"
+      >{{ registrationStatus ? companyName : 'Vous êtes...?' }}
+    </span>
+  </TeeDsfrButton>
+</template>
+<script setup lang="ts">
+import Breakpoint from '@/utils/breakpoints'
+
+interface Props {
+  registrationStatus: boolean
+}
+const props = defineProps<Props>()
+const emit = defineEmits(['click'])
+
+const badgeIcon = computed(() => {
+  if (Breakpoint.isSmallScreen() && !props.registrationStatus) {
+    return 'fr-bg--yellow fr-icon-question-mark'
+  } else {
+    return 'fr-bg--green fr-icon-check-line'
+  }
+})
+const companyName = 'La meilleure entreprise de France'
+</script>
+<style lang="scss" scoped>
+.register-icon-profile {
+  position: relative;
+  float: left;
+}
+
+#register-text {
+  text-overflow: ellipsis;
+  overflow: hidden;
+  max-width: 150px;
+  font-size: 0.875rem;
+  display: inherit;
+  white-space: nowrap;
+}
+
+.register-badge {
+  display: flex;
+  height: 1rem;
+  width: 1rem;
+  padding: 0.1rem;
+  position: absolute;
+  align-items: center;
+}
+
+.register-badge::before {
+  width: 0.8rem;
+}
+
+#badge-mobile {
+  right: -0.2px;
+  top: -4px;
+}
+
+#base-badge {
+  bottom: 16px;
+  left: 13px;
+}
+</style>
