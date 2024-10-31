cd884f2a8e6a3b9b15bd47eb5fe189913e62ebf6
Oumeima El Isbihani <31712244+oumeimaelisbihani@users.noreply.github.com>
2024-10-31 16:21:29 +0100

    Feature : Je complète mon profile sur la fiche projet (#1260)
    Ajout d'un composant TeeRegisterHighlight
    Changement condition de l'affichage du composant avec
    v-if="!CompanyDataStorage.hasData()"
    
    To do : brancher avec la modale (inject/provide déjà en place dans la
    branche siret-modale)
    
    #1132

---
+++ apps/web/src/components/identification/TeeRegisterHighlight.vue
@@ -0,0 +1,31 @@
+<template>
+  <DsfrHighlight
+    class="fr-highlight-border--yellow fr-highlight-bg--yellow--lightness fr-m-0 fr-p-0"
+    :large="true"
+  >
+    <template #default>
+      <div class="fr-container--fluid fr-p-4v">
+        <div class="fr-grid-row fr-grid-row--middle">
+          <img
+            class="fr-col-2 fr-col-xs-2 fr-mr-8v"
+            src="/images/tracks/ecriture.svg"
+            alt="image / ecriture"
+          />
+          <div class="fr-col-9 fr-col-xs-8">
+            <p class="fr-mb-2v">{{ text }}</p>
+            <TeeDsfrButton
+              class="fr-text--yellow fr-text--bold"
+              label="Je complète mon profil"
+            />
+          </div>
+        </div>
+      </div>
+    </template>
+  </DsfrHighlight>
+</template>
+<script setup lang="ts">
+interface Props {
+  text: string
+}
+defineProps<Props>()
+</script>
