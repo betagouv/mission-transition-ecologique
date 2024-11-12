d406802853a25c226e60d94c4d97564c4e801ef7
ttdm <thibaut.tezenas@gmail.com>
2024-11-12 09:15:54 +0100

    fix lint by deleting unused imports

--- apps/web/src/components/identification/TeeRegisterModal.vue
+++ apps/web/src/components/identification/TeeRegisterModal.vue
@@ -45,7 +45,7 @@
 </template>
 <script setup lang="ts">
 import Translation from '@/utils/translation'
-import { EstablishmentFront, CompanyDataStorageKey, CompanyDataType, LegalCategory, StructureSize } from '@/types'
+import { EstablishmentFront, CompanyDataStorageKey, CompanyDataType } from '@/types'
 import Breakpoint from '@/utils/breakpoints'
 import CompanyDataStorage from '@/utils/storage/companyDataStorage'
 import { onClickOutside } from '@vueuse/core'
