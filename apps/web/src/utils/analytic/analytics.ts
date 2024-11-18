2e851fa972ae50c8bb61eab4e9a92ec724ec308c
oumeimaelisbihani <elisbihani.oumeima17@hotmail.fr>
2024-11-18 17:55:17 +0100

    revert change localStorage for cookies

--- apps/web/src/utils/analytic/analytics.ts
+++ apps/web/src/utils/analytic/analytics.ts
@@ -4,7 +4,6 @@ import Matomo from './matomo'
 export default class Analytics {
   static sendEvent(action: string, name: string | null = null, value?: object) {
     Posthog.captureEvent(name ? name : 'unnamed event', value)
-
     Matomo.sendEvent(action, name, JSON.stringify(value))
   }
 }
