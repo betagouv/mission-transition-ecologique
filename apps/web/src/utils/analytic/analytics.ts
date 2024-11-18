383d6fc4432cf9524635c19b1c587ddf0a11f705
oumeimaelisbihani <elisbihani.oumeima17@hotmail.fr>
2024-11-18 10:30:14 +0100

    remove console

--- apps/web/src/utils/analytic/analytics.ts
+++ apps/web/src/utils/analytic/analytics.ts
@@ -3,7 +3,6 @@ import Matomo from './matomo'
 
 export default class Analytics {
   static sendEvent(action: string, name: string | null = null, value?: object) {
-    console.log(name, value)
     Posthog.captureEvent(name ? name : 'unnamed event', value)
 
     Matomo.sendEvent(action, name, JSON.stringify(value))
