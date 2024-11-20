1a10e6de8d08f3f008600ba8c7b76059bfe383b3
dlemoine <contact@dorianlemoine.fr>
2024-11-20 11:48:15 +0100

    fix: Refactor program eligibility checks for clarity
    Updated the `isPartiallyEligible` method to include a more explicit check for the number of years of activity criterion. Refactored the `isYearsEligibleForAll` method to use `includes` for string containment, enhancing readability and correctness.

--- libs/data/src/program/programEligibility.ts
+++ libs/data/src/program/programEligibility.ts
@@ -4,10 +4,13 @@ export class ProgramEligibility {
   public static readonly ELIGIBLE_FOR_ALL = 'Éligible à toutes les entreprises'
 
   public static isPartiallyEligible(program: ProgramType) {
-    return program["conditions d'éligibilité"]["autres critères d'éligibilité"] || !this.isYearsEligibleForAll(program)
+    return (
+      program["conditions d'éligibilité"]["autres critères d'éligibilité"] ||
+      (program["conditions d'éligibilité"]["nombre d'années d'activité"] && !this.isYearsEligibleForAll(program))
+    )
   }
 
   private static isYearsEligibleForAll(program: ProgramType) {
-    return this.ELIGIBLE_FOR_ALL in program["conditions d'éligibilité"]["nombre d'années d'activité"]
+    return program["conditions d'éligibilité"]["nombre d'années d'activité"].includes(this.ELIGIBLE_FOR_ALL)
   }
 }
