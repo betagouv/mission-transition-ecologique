db1021a29cb4ff2f938c9928a0ab33f46a56f460
oumeimaelisbihani <elisbihani.oumeima17@hotmail.fr>
2024-11-21 15:13:49 +0100

    search wip

--- libs/backend-ddd/src/establishment/infrastructure/json/nafJsonMapping.ts
+++ libs/backend-ddd/src/establishment/infrastructure/json/nafJsonMapping.ts
@@ -25,10 +25,19 @@ export class NafJsonMapping implements NafMappingType {
         .replace(/[\u0300-\u036f]/g, '')
         .replace(/[-\s]/g, '')
         .trim()
-    
+    const normalizedSearch = normalizeString(searchTerm)
+    const regex = /\b\d{2}\.\d{2}[A-Z]?\b/
+    const isNAFCodeSearch = regex.test(searchTerm)
+    let results = []
+    if (isNAFCodeSearch) {
+      results = nafMapping.filter((pair: { NIV5: string }) => pair.NIV5 === normalizedSearch)
+    } else {
+      if (searchTerm.length === 1){ 
+        results = nafMapping.filter((pair: { NIV1: string }) => pair.NIV1 === normalizedSearch)
+      } 
+    } 
     return nafMapping.filter((pair: { nom: string }) => {
       const normalizedCity = normalizeString(pair.nom)
-      const normalizedSearch = normalizeString(searchValue)
 
       return normalizedCity.startsWith(normalizedSearch)
     })
