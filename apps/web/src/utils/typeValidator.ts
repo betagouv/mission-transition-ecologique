fdb5688fd2d72cd8af19dfb7f350e5fdf1ce7e2b
dlemoine <contact@dorianlemoine.fr>
2024-12-05 20:38:24 +0100

    refactor: Refactor method parameter for clarity.
    Renamed the parameter `interfaceObj` to `type` in the `isOfType` method for improved clarity and consistency with the method's intent. This change helps better convey the purpose of the parameter in the context of type validation.

--- apps/web/src/utils/typeValidator.ts
+++ apps/web/src/utils/typeValidator.ts
@@ -1,5 +1,5 @@
 export class TypeValidator {
-  static isOfType<T>(value: any, interfaceObj: T): value is T {
-    return Object.keys(interfaceObj as object).every((key) => key in value)
+  static isOfType<T>(value: any, type: T): value is T {
+    return Object.keys(type as object).every((key) => key in value)
   }
 }
