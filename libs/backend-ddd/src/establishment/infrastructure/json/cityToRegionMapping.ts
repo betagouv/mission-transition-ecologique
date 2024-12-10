1e6a3bedb176ac12f322c36d957da8eb8f4d3066
oumeimaelisbihani <elisbihani.oumeima17@hotmail.fr>
2024-12-10 16:38:29 +0100

    revert 3

--- libs/backend-ddd/src/establishment/infrastructure/json/cityToRegionMapping.ts
+++ libs/backend-ddd/src/establishment/infrastructure/json/cityToRegionMapping.ts
@@ -1,12 +1,12 @@
-import { communes } from '@tee/data/references'
+import { regionMapping } from '@tee/data/references'
 import { Maybe } from 'true-myth'
 import { CityToRegionMappingType } from '../../domain/spi'
-import { Commune } from '@tee/common'
+
 export class CityToRegionMapping implements CityToRegionMappingType {
   public getRegion(cityCode: string): Maybe<string> {
-    const cityRegionPair = (communes as Commune[]).find(
-      (pair: { code: string; region: { code: string; nom: string } }) => pair.code === cityCode
+    const cityRegionPair = regionMapping.find(
+      (pair: { COM: string; REGION: string } | { COM: string; REGION: null }) => pair.COM === cityCode
     )
-    return Maybe.of(cityRegionPair?.region.nom)
+    return Maybe.of(cityRegionPair?.REGION)
   }
 }
