5b93d68fd71cd4e9e84e29288110db8f6f17d6c1
dlemoine <contact@dorianlemoine.fr>
2025-01-08 10:31:56 +0100

    refactor: Refactor `getRegion` to use zipCode instead of cityCode.
    Updated the `getRegion` method and relevant calls to replace `cityCode` with `zipCode`, ensuring more accurate mapping. Adjusted the logic in `CityToRegionMapping` to handle zip codes and maintain compatibility with the existing data structure.

--- libs/backend-ddd/src/establishment/infrastructure/json/cityToRegionMapping.ts
+++ libs/backend-ddd/src/establishment/infrastructure/json/cityToRegionMapping.ts
@@ -3,9 +3,9 @@ import { Maybe } from 'true-myth'
 import { CityToRegionMappingType } from '../../domain/spi'
 import { Commune } from '@tee/common'
 export class CityToRegionMapping implements CityToRegionMappingType {
-  public getRegion(cityCode: string): Maybe<string> {
-    const cityRegionPair = (communes as Commune[]).find(
-      (pair: { code: string; region: { code: string; nom: string } }) => pair.code === cityCode
+  public getRegion(zipCode: string): Maybe<string> {
+    const cityRegionPair = (communes as Commune[]).find((pair: { codesPostaux: string[]; region: { code: string; nom: string } }) =>
+      pair.codesPostaux.includes(zipCode)
     )
     return Maybe.of(cityRegionPair?.region.nom)
   }
