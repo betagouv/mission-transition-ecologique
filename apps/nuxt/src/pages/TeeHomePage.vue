43ba798cdf9140a68c9f520b458235a855e3e450
oumeimaelisbihani <elisbihani.oumeima17@hotmail.fr>
2025-01-09 14:44:35 +0100

    align

--- apps/nuxt/src/pages/TeeHomePage.vue
+++ apps/nuxt/src/pages/TeeHomePage.vue
@@ -6,70 +6,73 @@
         <TeeCta />
       </div>
     </div>
-    <h1 class="fr-container fr-text--blue-france fr-py-8v">Quel est votre projet ?</h1>
-    <CatalogProjects
-      :show-breadcrumbs="false"
-      :show-title="false"
-      :show-limit="9"
-      :show-counter="false"
-    />
-    <div class="fr-container fr-grid-row fr-grid-row--center fr-pt-8v">
-      <div class="fr-col-12 fr-col-justify--center">
-        <TeeButtonLink
-          :to="{ name: RouteName.CatalogProjects }"
-          size="sm"
-          icon="fr-icon-arrow-right-line"
-          icon-right
-          class="no-outline fr-text--decoration-underline fr-mb-8v"
-        >
-          Voir tout les projets
-        </TeeButtonLink>
-      </div>
-      <div class="fr-bg--blue-france--lightness fr-col-12 fr-py-0-5v fr-my-8v"></div>
-      <div class="fr-col-6 fr-px-10w fr-text-center fr-position--relative">
-        <img
-          src="/images/yellow-circle.svg"
-          class="circle-background fr-position--absolute"
-          alt="yellow-circle-background"
-        />
-        <h4 class="fr-text--blue-france fr-pb-2v">
-          Un point d'entrée unique pour toutes les aides à la transition écologique des entreprises
-        </h4>
-        <p>
-          Transition écologique des entreprises rassemble plus de <b>150 aides</b> nationales, régionales et territoriales
-          <b>mises à jour quotidiennement</b> issues de l'ensemble des <b>partenaires publiques</b>: ADEME, Bpifrance, CCI, CMA, etc.
-        </p>
-        <div class="fr-col-justify--center fr-col-content--middle">
-          <img
-            v-for="operator in operators"
-            :key="operator.label"
-            :src="operator.img"
-            :alt="operator.label"
-            width="70px"
-            class="fr-m-2v"
-          />
+    <div class="fr-container fr-py-2v fr-px-5v fr-px-sm-8v fr-px-md-20v">
+      <h1 class="fr-text--blue-france fr-py-8v">Quel est votre projet ?</h1>
+      <CatalogProjects
+        :show-breadcrumbs="false"
+        :show-title="false"
+        :show-limit="9"
+        :show-counter="false"
+      />
+      <div class="fr-container fr-grid-row fr-grid-row--center fr-pt-8v">
+        <div class="fr-col-12 fr-col-justify--center">
+          <TeeButtonLink
+            :to="{ name: RouteName.CatalogProjects }"
+            size="sm"
+            icon="fr-icon-arrow-right-line"
+            icon-right
+            class="no-outline fr-text--decoration-underline fr-mb-8v"
+          >
+            Voir tout les projets
+          </TeeButtonLink>
         </div>
-      </div>
-      <div class="fr-col-6 fr-px-10w fr-text-center fr-position--relative">
-        <img
-          src="/images/green-circle.svg"
-          class="circle-background fr-position--absolute"
-          alt="green-circle-background"
-        />
-        <h4 class="fr-text--blue-france fr-pb-2v">Des conseillers pour vous aider sur l'ensemble du territoire</h4>
-        <p>
-          Conseillers-entreprises vous met en relation avec <b>le conseiller compétent pour votre demande sur votre territoire</b>. Des
-          spécialistes pour vous accompagner dans vos réflexions, et vos recherches pour mener à bien votre projet de transition écologique.
-        </p>
-        <div class="fr-col-justify--center fr-col-content--middle">
+        <div class="fr-bg--blue-france--lightness fr-col-12 fr-py-0-5v fr-my-8v"></div>
+        <div class="fr-col-6 fr-px-10w fr-text-center fr-position--relative">
           <img
-            v-for="operator in otherOperators"
-            :key="operator.label"
-            :src="operator.img"
-            :alt="operator.label"
-            :width="operator.width"
-            class="fr-m-2v"
+            src="/images/yellow-circle.svg"
+            class="circle-background fr-position--absolute"
+            alt="yellow-circle-background"
           />
+          <h4 class="fr-text--blue-france fr-pb-2v">
+            Un point d'entrée unique pour toutes les aides à la transition écologique des entreprises
+          </h4>
+          <p>
+            Transition écologique des entreprises rassemble plus de <b>150 aides</b> nationales, régionales et territoriales
+            <b>mises à jour quotidiennement</b> issues de l'ensemble des <b>partenaires publiques</b>: ADEME, Bpifrance, CCI, CMA, etc.
+          </p>
+          <div class="fr-col-justify--center fr-col-content--middle">
+            <img
+              v-for="operator in operators"
+              :key="operator.label"
+              :src="operator.img"
+              :alt="operator.label"
+              width="70px"
+              class="fr-m-2v"
+            />
+          </div>
+        </div>
+        <div class="fr-col-6 fr-px-10w fr-text-center fr-position--relative">
+          <img
+            src="/images/green-circle.svg"
+            class="circle-background fr-position--absolute"
+            alt="green-circle-background"
+          />
+          <h4 class="fr-text--blue-france fr-pb-2v">Des conseillers pour vous aider sur l'ensemble du territoire</h4>
+          <p>
+            Conseillers-entreprises vous met en relation avec <b>le conseiller compétent pour votre demande sur votre territoire</b>. Des
+            spécialistes pour vous accompagner dans vos réflexions, et vos recherches pour mener à bien votre projet de transition
+            écologique.
+          </p>
+          <div class="fr-col-justify--center fr-col-content--middle">
+            <img
+              v-for="operator in otherOperators"
+              :key="operator.label"
+              :src="operator.img"
+              :alt="operator.label"
+              :width="operator.width"
+              class="fr-m-2v"
+            />
+          </div>
         </div>
       </div>
     </div>
