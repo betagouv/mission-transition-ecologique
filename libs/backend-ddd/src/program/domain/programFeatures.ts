e12c432d3aa394f207883edc8a980189b0d25483
ttdm <thibaut.tezenas@gmail.com>
2025-01-13 14:34:20 +0100

    revert, reactivate the warning in ProgramFeatures

--- libs/backend-ddd/src/program/domain/programFeatures.ts
+++ libs/backend-ddd/src/program/domain/programFeatures.ts
@@ -4,6 +4,7 @@ import { sortPrograms } from './sortPrograms'
 import { Result } from 'true-myth'
 import { ProgramEligibilityType, ProgramType, ProgramTypeWithEligibility } from '@tee/data'
 import { Objective, QuestionnaireData } from '@tee/common'
+import { Monitor } from '../../common'
 import ProgramCustomizer from './programCustomizer'
 import { ProgramNotFoundError } from './types'
 
@@ -29,6 +30,7 @@ export default class ProgramFeatures {
   public getOneWithMaybeEligibility(id: string, questionnaireData: QuestionnaireData): Result<ProgramTypeWithEligibility, Error> {
     let program = this.getById(id)
     if (!program) {
+      Monitor.warning('Requested Program Id unknown', { id })
       return Result.err(new ProgramNotFoundError())
     }
     if (new ProgramCustomizer().shouldRewritePrograms(questionnaireData)) {
