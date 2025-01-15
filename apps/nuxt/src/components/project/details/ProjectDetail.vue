e6f125ed3f91d383406899cfae697cc48039b57a
Oumeima El Isbihani <31712244+oumeimaelisbihani@users.noreply.github.com>
2025-01-15 17:05:59 +0100

    Fix: couleur de la thématique dans les exemples de projet depuis une aide (#1503)
    utilisation de la couleur du projet

--- apps/nuxt/src/components/project/details/ProjectDetail.vue
+++ apps/nuxt/src/components/project/details/ProjectDetail.vue
@@ -43,7 +43,7 @@ useSeoMeta(MetaSeo.get(project.value?.title, project.value?.shortDescription, pr
 
 if (project.value) {
   const selectedThemeId = Theme.getThemeFromSelectedOrPriorityTheme()
-  const themeId = selectedThemeId.value ? project.value?.themes.find((theme) => theme === selectedThemeId.value) : project.value?.mainTheme
+  const themeId = project.value?.themes.find((theme) => theme === selectedThemeId.value) || project.value?.mainTheme
   themeColor.value = Theme.getColorById(themeId as ThemeId)
 }
 
