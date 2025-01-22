1597cf3a388eabfa099a54b8bce6639b8c835381
Yohann Valentin <pro@yohannvalentin.com>
2025-01-22 21:58:58 +0100

    fix: matomo on non prod env. And homepage project trigger when change company data state

--- apps/nuxt/nuxt.scripts.config.ts
+++ apps/nuxt/nuxt.scripts.config.ts
@@ -3,7 +3,6 @@ import Config from './src/config'
 
 export class NuxtScriptsConfig {
   static getRegistry(): NuxtConfigScriptRegistry | undefined {
-    console.log('NuxtScriptsConfig.getRegistry(): ' + Config.hasMatomo())
     if (Config.hasMatomo()) {
       return {
         matomoAnalytics: true
