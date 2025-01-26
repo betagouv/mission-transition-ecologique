33dd6ceac7e47ef14a94d4c6fbf6d094205e3844
ttdm <thibaut.tezenas@gmail.com>
2025-01-26 19:20:10 +0100

    fix communes, ajoute un event posthog

--- libs/data/tests/data.test.ts
+++ libs/data/tests/data.test.ts
@@ -2,7 +2,7 @@ import Ajv from 'ajv/dist/2020'
 import { ValidateFunction } from 'ajv'
 import programSchema from '../schemas/program-with-publicodes-schema.json'
 import regionSchema from '../schemas/region-data-schema.json'
-import communes from '../references/communes.json'
+import communes from '../static/communes.json'
 import { prependInterface, readPrograms } from '../src/program/dataPipeline'
 import Engine from 'publicodes'
 
