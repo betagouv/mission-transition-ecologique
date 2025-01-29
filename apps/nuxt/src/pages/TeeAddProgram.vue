f08d14e4e7db4150ece6269280934c5f0636df22
oumeimaelisbihani <elisbihani.oumeima17@hotmail.fr>
2025-01-29 15:30:09 +0100

    remove unecessary typeform

--- apps/nuxt/src/pages/TeeAddProgram.vue
+++ apps/nuxt/src/pages/TeeAddProgram.vue
@@ -12,7 +12,6 @@
 <script setup lang="ts">
 import { defineRouteRules } from '#imports'
 import { RouteName } from '@/types'
-import { onMounted, onBeforeUnmount } from 'vue'
 
 definePageMeta({
   path: '/ajouter-une-aide-entreprises',
@@ -25,25 +24,6 @@ defineRouteRules({
     changefreq: 'monthly'
   }
 })
-
-if (import.meta.client) {
-  const typeformScript = document.createElement('script')
-  typeformScript.async = true
-  typeformScript.setAttribute('type', 'text/javascript')
-  typeformScript.setAttribute('src', 'https://embed.typeform.com/next/embed.js')
-
-  onMounted(() => {
-    document.head.appendChild(typeformScript)
-  })
-
-  onBeforeUnmount(() => {
-    document.head.removeChild(typeformScript)
-    const typeformLink = document.head.querySelector('link[href*="https://embed.typeform.com"]')
-    if (typeformLink) {
-      document.head.removeChild(typeformLink)
-    }
-  })
-}
 </script>
 <style lang="scss" scoped>
 .tee-baserow-form {
