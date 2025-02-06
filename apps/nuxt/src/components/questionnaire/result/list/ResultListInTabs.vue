8b43a94cf6d0a29ece41c38e330c06010bcce733
oumeimaelisbihani <oumeima.el-isbihani@multi.coop>
2025-02-06 18:29:22 +0100

    remove console

--- apps/nuxt/src/components/questionnaire/result/list/ResultListInTabs.vue
+++ apps/nuxt/src/components/questionnaire/result/list/ResultListInTabs.vue
@@ -65,7 +65,6 @@ const navigationStore = useNavigationStore()
 const programStore = useProgramStore()
 const { projects, hasError: hasErrorProjects } = storeToRefs(useProjectStore())
 const { programs, hasError: hasErrorPrograms } = storeToRefs(programStore)
-console.log(programs.value)
 const { tabSelectedOnList } = storeToRefs(navigationStore)
 
 onNuxtReady(async () => {
@@ -89,7 +88,6 @@ const titles: TeeDsfrTabsProps['tabTitles'] = [
 const filteredPrograms = computed(() => {
   return programs.value ? programStore.getProgramsByFilters(programs.value) : undefined
 })
-console.log(filteredPrograms.value)
 const filteredProjects = computed(() => {
   if (!projects.value) {
     return undefined
