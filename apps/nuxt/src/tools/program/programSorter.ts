0eaad0b0530a2abbc633ccbb1c26185a564f1970
dlemoine <contact@dorianlemoine.fr>
2025-02-06 17:23:14 +0100

    refactor: extract program sorting logic into specific class
    Moved sorting logic to dedicated utility class. Improved clarity by replacing inline functionality with `ProgramFilter` and `ProgramSorter`.

---
+++ apps/nuxt/src/tools/program/programSorter.ts
@@ -0,0 +1,18 @@
+import { ProgramAidType, ProgramData } from '@/types'
+
+export class ProgramSorter {
+  static byFinanceAidType(programs: ProgramData[]) {
+    return programs.slice().sort((a, b) => {
+      const aidKey = "nature de l'aide"
+      switch (true) {
+        case a[aidKey] === ProgramAidType.fund && a[aidKey] !== b[aidKey]:
+        case a[aidKey] === ProgramAidType.loan && b[aidKey] !== ProgramAidType.fund:
+          return -1
+        case a[aidKey] === b[aidKey]:
+          return 0
+        default:
+          return 1
+      }
+    })
+  }
+}
