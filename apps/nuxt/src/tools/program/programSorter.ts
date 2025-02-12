a3e072d8cf1b3f6e979b4d9fb318b9ceb566f721
dlemoine <contact@dorianlemoine.fr>
2025-02-12 14:47:44 +0100

    refactor: simplify program sorting logic in programSorter

--- apps/nuxt/src/tools/program/programSorter.ts
+++ apps/nuxt/src/tools/program/programSorter.ts
@@ -5,11 +5,11 @@ export class ProgramSorter {
     return programs.slice().sort((a, b) => {
       const aidKey = "nature de l'aide"
       switch (true) {
-        case a[aidKey] === ProgramAidType.fund && a[aidKey] !== b[aidKey]:
-        case a[aidKey] === ProgramAidType.loan && b[aidKey] !== ProgramAidType.fund:
-          return -1
         case a[aidKey] === b[aidKey]:
           return a.titre.localeCompare(b.titre)
+        case a[aidKey] === ProgramAidType.fund:
+        case a[aidKey] === ProgramAidType.loan && b[aidKey] !== ProgramAidType.fund:
+          return -1
         default:
           return 1
       }
