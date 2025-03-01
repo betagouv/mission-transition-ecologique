a98c9554492de7e1b016005bd8fc0abd88034954
oumeimaelisbihani <oumeima.el-isbihani@multi.coop>
2025-03-01 18:01:21 +0100

    review changes

--- apps/nuxt/src/error.vue
+++ apps/nuxt/src/error.vue
@@ -1,12 +1,18 @@
 <template>
   <NuxtLayout>
     <div class="fr-container">
-      <div class="fr-my-7w fr-mt-md-12w fr-mb-md-10w fr-grid-row fr-grid-row--gutters fr-grid-row--middle fr-grid-row--left">
+      <div class="fr-my-7w fr-mt-md-8w fr-mb-md-10w fr-grid-row fr-grid-row--gutters fr-grid-row--middle fr-grid-row--left">
         <div class="fr-py-0 fr-col-12 fr-col-md-6">
           <h1>Page non trouvée</h1>
           <p class="fr-text--sm fr-mb-3w">{{ `Erreur ${error.statusCode}` }}</p>
-          <p class="fr-text--lead fr-mb-3w">{{ leadText }}</p>
-          <p class="fr-text--sm fr-mb-5w">{{ errorText }}</p>
+          <p
+            class="fr-text--lead fr-mb-3w"
+            v-html="leadText"
+          ></p>
+          <p
+            class="fr-text--sm fr-mb-5w"
+            v-html="errorText"
+          ></p>
           <TeeDsfrButton
             v-if="error.statusCode === 404"
             label="Page d'accueil"
@@ -14,7 +20,7 @@
           />
           <ContactButton v-else />
         </div>
-        <div class="fr-col-8 fr-col-justify--left fr-col-sm-6 fr-col-md-3 fr-col-offset-md-1 fr-px-6w fr-px-md-0 fr-pt-4v fr-py-md-0">
+        <div class="fr-col-6 fr-col-justify--left fr-col-sm-6 fr-col-md-3 fr-col-offset-md-1 fr-px-0 fr-pt-4v fr-py-md-0">
           <img
             class="fr-footer__logo fr-responsive-img"
             src="/images/tracks/no-results.png"
@@ -43,12 +49,12 @@ const toHomePage = async () => {
   await router.push(routeToBaseList)
 }
 const error404Text =
-  "Si vous avez tapé l'adresse web dans le navigateur, vérifiez qu'elle est correcte. La page n’est peut-être plus disponible. Dans ce cas, pour continuer votre visite vous pouvez consulter notre page d’accueil. Sinon contactez-nous pour que l’on puisse vous rediriger vers la bonne information."
+  "Si vous avez tapé l'adresse web dans le navigateur, vérifiez qu'elle est correcte. La page n’est peut-être plus disponible.<br/> Pour continuer votre visite nous vous invitons à consulter notre page d’accueil."
 const error500Text = 'Essayez de rafraîchir la page ou bien réessayez plus tard.'
 
 const leadText = computed<string>(() => {
   if (props.error.statusCode === 404) {
-    return 'La page que vous cherchez est introuvable. Excusez-nous pour la gène occasionnée.'
+    return `La page que vous cherchez est introuvable.<br/> Excusez-nous pour la gène occasionnée.`
   } else {
     return 'Désolé, le service rencontre un problème, nous travaillons pour le résoudre le plus rapidement possible'
   }
