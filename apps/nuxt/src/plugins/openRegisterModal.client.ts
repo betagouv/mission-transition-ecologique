1f5f828cd8fc4ab8b4cbb431e26fe34fa7e42420
ttdm <thibaut.tezenas@gmail.com>
2025-03-03 16:09:48 +0100

    remove posthog import

--- apps/nuxt/src/plugins/openRegisterModal.client.ts
+++ apps/nuxt/src/plugins/openRegisterModal.client.ts
@@ -1,10 +1,7 @@
-import Posthog from '@/tools/analytic/posthog'
 import { CompanyData } from '@/tools/companyData'
 import Navigation from '@/tools/navigation'
 
 export default defineNuxtPlugin(() => {
-  Posthog.install()
-
   const router = useRouter()
   router.afterEach((to, from, failure) => {
     if (!failure) {
