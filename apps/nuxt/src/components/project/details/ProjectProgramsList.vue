86af441cc3342427b0cb715983533e025aef43f9
Yohann Valentin <pro@yohannvalentin.com>
2025-03-03 17:24:53 +0100

    feat: improve layout and structure of LinkedProjects and ProjectProgramsList components

--- apps/nuxt/src/components/project/details/ProjectProgramsList.vue
+++ apps/nuxt/src/components/project/details/ProjectProgramsList.vue
@@ -1,19 +1,22 @@
 <template>
   <ul>
-    <li class="fr-mb-2v fr-text--purple fr-text--bold">{{ title }}</li>
+    <li class="fr-text--purple">
+      <h4 class="fr-mb-2v fr-text--purple fr-text--bold fr-text--md">{{ title }}</h4>
+      <ul class="fr-raw-list fr-ml-n2w">
+        <li
+          v-for="program in programs"
+          :key="program.id"
+          class="fr-col-12 no-outline fr-mb-6v"
+        >
+          <ProgramCard
+            :program="program"
+            :project="project"
+            class="fr-enlarge-link fr-card--horizontal-tier"
+          />
+        </li>
+      </ul>
+    </li>
   </ul>
-
-  <div
-    v-for="program in programs"
-    :key="program.id"
-    class="fr-col-12 no-outline fr-mb-6v"
-  >
-    <ProgramCard
-      :program="program"
-      :project="project"
-      class="fr-enlarge-link fr-card--horizontal-tier"
-    />
-  </div>
 </template>
 
 <script setup lang="ts">
