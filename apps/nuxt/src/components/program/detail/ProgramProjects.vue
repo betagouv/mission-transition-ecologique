d9f826e031d2633727a5439f527d36c53e4627d1
Yohann Valentin <pro@yohannvalentin.com>
2025-03-05 17:11:46 +0100

    style: update layout and spacing in ProgramProjects component

--- apps/nuxt/src/components/program/detail/ProgramProjects.vue
+++ apps/nuxt/src/components/program/detail/ProgramProjects.vue
@@ -1,19 +1,17 @@
 <template>
-  <div class="fr-mt-2v">
-    <ul class="fr-grid-row fr-grid-row--center fr-grid-row-md--left fr-raw-list">
-      <li
-        v-for="(linkedProject, index) in linkedProjects"
-        :key="linkedProject.id"
-        :class="index === 0 ? `fr-mr-2v` : index === linkedProjects.length - 1 ? `fr-ml-2v` : `fr-mx-2v`"
-      >
-        <TeeProjectButton
-          :project="linkedProject"
-          target="_blank"
-          class="fr-my-1-5v"
-        />
-      </li>
-    </ul>
-  </div>
+  <ul class="fr-grid-row fr-grid-row--center fr-grid-row-md--left fr-raw-list">
+    <li
+      v-for="(linkedProject, index) in linkedProjects"
+      :key="linkedProject.id"
+      :class="index === 0 ? `fr-mr-2v` : index === linkedProjects.length - 1 ? `fr-ml-2v` : `fr-mx-2v`"
+    >
+      <TeeProjectButton
+        :project="linkedProject"
+        target="_blank"
+        class="fr-mb-1-5v"
+      />
+    </li>
+  </ul>
 </template>
 <script lang="ts" setup>
 import type { ProjectType as ProjectType } from '@/types'
