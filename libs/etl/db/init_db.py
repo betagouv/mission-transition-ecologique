f480e5ebfffefcacb678d76d806f06ffc5e4fd08
ttdm <thibaut.tezenas@beta.gouv.fr>
2025-03-12 19:20:51 +0100

    Feat: etl library to handle the database updates (#1669)
    Ajout d'une librarie pour gérer la base de donnée. 
    Librairie en python, séparée du reste du repo et isolée via docker. 
    
    close #1621

---
+++ libs/etl/db/init_db.py
@@ -0,0 +1,29 @@
+import psycopg2
+from etl.tools.db_manager import DBManager
+from dotenv import load_dotenv
+import os
+
+def init_db():
+    """Executes schema.sql to initialize the database."""
+    try:
+        with open("db/schema.sql", "r") as f:
+            sql_script = f.read()
+            DBManager().execute_script(sql_script)
+
+    except Exception as e:
+        print(f"❌ Error initializing database: {e}")
+
+    load_dotenv()
+    if os.getenv("TEST") == "True":
+        DBManager().query("GRANT ALL PRIVILEGES ON SCHEMA statistics_test TO PUBLIC;")
+        DBManager().query(
+            "GRANT INSERT, DELETE, UPDATE, TRUNCATE, REFERENCES, TRIGGER, SELECT ON ALL TABLES IN SCHEMA statistics_test TO PUBLIC;"
+        )
+
+
+def reset_test_db():
+    DBManager().query("DROP SCHEMA statistics_test CASCADE;")
+
+
+if __name__ == "__main__":
+    init_db()
