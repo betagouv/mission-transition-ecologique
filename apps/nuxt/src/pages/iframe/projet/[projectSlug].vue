06586069d6863d160d93831ada4da2037a15a85e
ttdm <thibaut.tezenas@gmail.com>
2025-03-13 18:20:00 +0100

    functional proposition

--- apps/nuxt/src/pages/iframe/projet/[projectSlug].vue
+++ apps/nuxt/src/pages/iframe/projet/[projectSlug].vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { MiddlewareName } from '@/middleware/type/middlewareName'
+import Analytics from '@/tools/analytic/analytics'
 // import Analytics from '@/tools/analytic/analytics'
 import { RouteName } from '@/types'
 
@@ -17,20 +18,14 @@ const router = useRouter()
 const href = router.resolve({ name: RouteName.CatalogProjectDetail, params: { projectSlug: currentProject.value?.slug } }).href
 
 if (import.meta.client) {
-  console.log(window.location.href)
+  const params = new URLSearchParams(window.location.search)
+  const parentUrl = params.get('parent_url')
+  Analytics.sendEvent('iframe_view', {
+    type: 'project',
+    title: title,
+    referrer_url: parentUrl
+  })
 }
-// console.log({
-//   type: 'project',
-//   title: title,
-//   host_url: window.location.href
-// })
-// if (import.meta.client) {
-//   Analytics.sendEvent('iframe_view', 'iframe_view', {
-//     type: 'project',
-//     title: title,
-//     host_url: window.location.href
-//   })
-// }
 </script>
 
 <template>
