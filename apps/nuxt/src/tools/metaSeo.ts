c4ace3c7b3793154c788513b929af06c9b6cdcfe
ttdm <thibaut.tezenas@gmail.com>
2025-04-07 00:32:02 +0200

    feat: social network sharings improved, v0 to test

--- apps/nuxt/src/tools/metaSeo.ts
+++ apps/nuxt/src/tools/metaSeo.ts
@@ -2,10 +2,10 @@ import { UseSeoMetaInput } from '@unhead/vue'
 
 export class MetaSeo {
   private static readonly _baseTitle = 'Transition écologique des TPE & PME'
-  private static readonly _defaultTitle = 'Transition écologique - Aides et financements TPE & PME'
-  private static readonly _defaultImage = '/images/TEE-preview-image-529-by-298.png'
+  private static readonly _defaultTitle = 'Aides et financements personnalisés'
+  private static readonly _defaultImage = '/images/TEE-social-network-sharing.webp'
   private static readonly _defaultDescription =
-    'Service public pour les entreprises : Accédez simplement aux aides, accompagnements et financements pour réduire votre impact environnemental.'
+    'Service public pour les entreprises : Accédez simplement aux aides, accompagnements et financements disponibles pour les entreprises de votre secteur'
 
   static readonly get = (title?: string, description?: string, image?: string): UseSeoMetaInput => {
     return this._buildMeta(this._title(title), this._image(image), this._description(description))
@@ -16,7 +16,7 @@ export class MetaSeo {
   }
 
   private static readonly _title = (title?: string) => {
-    return title ? `${this._baseTitle} - ${title}` : this._defaultTitle
+    return `${this._baseTitle} - ${title || this._defaultTitle}`
   }
 
   private static readonly _description = (description?: string) => {
