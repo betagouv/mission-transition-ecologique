a8d7280f95fbc0594b061cb7f2be6172d99c0665
Oumeima El Isbihani <31712244+oumeimaelisbihani@users.noreply.github.com>
2025-04-07 14:38:03 +0200

    Bug détail projet: section "mes aides et contacts" s'affichent dans le menu à gauche même s'il n'y a rien dedans  (#1792)
    pour fixer le cas où on a ni aides ni formulaire de contact :
    https://tee-preprod-pr1792.osc-fr1.scalingo.io/projets-entreprise/materiaux-de-reemploi
    au lieu de
    https://tee-preprod.osc-fr1.scalingo.io/projets-entreprise/materiaux-de-reemploi?
    
    ---------
    
    Co-authored-by: Yohann Valentin <pro@yohannvalentin.com>

--- apps/nuxt/src/components/project/details/ProjectSideNav.vue
+++ apps/nuxt/src/components/project/details/ProjectSideNav.vue
@@ -25,7 +25,9 @@ interface Props {
 }
 const props = defineProps<Props>()
 
-const menuItems = computed(() => allMenuItems.filter((item) => item.condition !== false))
+const { isDataFull } = storeToRefs(useCompanyDataStore())
+
+const menuItems = computed(() => allMenuItems.value.filter((item) => item.condition))
 
 const scrollTo = (id: string) => {
   const element = document.getElementById(id)
@@ -34,7 +36,7 @@ const scrollTo = (id: string) => {
   }
 }
 
-const allMenuItems = [
+const allMenuItems = computed(() => [
   {
     id: 'project',
     to: `project-description-details-title`,
@@ -47,13 +49,13 @@ const allMenuItems = [
     text: 'Pour aller plus loin',
     condition: props.project.moreDescription.length > 0
   },
-  { id: 'aids', to: `project-aids-title`, text: 'Mes aides' },
-  { id: 'contact', to: `form-title`, text: 'Contact' },
+  { id: 'aids', to: `project-aids-title`, text: 'Mes aides', condition: props.project.programs.length > 0 || isDataFull.value },
+  { id: 'contact', to: `form-title`, text: 'Contact', condition: props.project.programs.length > 0 || isDataFull.value },
   {
     id: 'linked-project',
     to: `project-linked-projects-title`,
     text: 'Prérequis',
     condition: props.project.linkedProjects.length > 0
   }
-]
+])
 </script>
