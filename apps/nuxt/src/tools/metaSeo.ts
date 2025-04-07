f490095af701f45472975d1508a658213a4aeffa
ttdm <thibaut.tezenas@gmail.com>
2025-04-07 09:13:09 +0200

    change image on program and project detial pages

--- apps/nuxt/src/tools/metaSeo.ts
+++ apps/nuxt/src/tools/metaSeo.ts
@@ -7,12 +7,12 @@ export class MetaSeo {
   private static readonly _defaultDescription =
     'Service public pour les entreprises : Accédez simplement aux aides, accompagnements et financements disponibles pour les entreprises de votre secteur'
 
-  static readonly get = (title?: string, description?: string, image?: string): UseSeoMetaInput => {
-    return this._buildMeta(this._title(title), this._image(image), this._description(description))
+  static readonly get = (title?: string, description?: string): UseSeoMetaInput => {
+    return this._buildMeta(this._title(title), this._defaultImage, this._description(description))
   }
 
   static readonly default = () => {
-    return this._buildMeta(this._title(), this._image(), this._defaultDescription)
+    return this._buildMeta(this._title(), this._defaultImage, this._defaultDescription)
   }
 
   private static readonly _title = (title?: string) => {
@@ -21,13 +21,6 @@ export class MetaSeo {
 
   private static readonly _description = (description?: string) => {
     return description ?? undefined
-  }
-
-  private static readonly _image = (image?: string) => {
-    if (!image) {
-      return this._defaultImage
-    }
-    return image?.charAt(0) === '/' ? image : `/${image}`
   }
 
   private static _buildMeta(title: string, image: string, description?: string): UseSeoMetaInput {
