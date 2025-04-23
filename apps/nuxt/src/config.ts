830fd125379791a318ed9c491751f8205f080f49
ttdm <thibaut.tezenas@gmail.com>
2025-04-23 18:03:21 +0200

    fix proposition using a new env variable

--- apps/nuxt/src/config.ts
+++ apps/nuxt/src/config.ts
@@ -1,10 +1,20 @@
 // @ts-expect-error relative path to import configCommon because of nuxt.config.ts .
 // eslint-disable-next-line @nx/enforce-module-boundaries
 import ConfigCommon from '../../../libs/common/src/config/configCommon'
+import { Environment } from '@tee/common'
 // eslint-disable-next-line @nx/enforce-module-boundaries
 
 export default class Config extends ConfigCommon {
   protected static override _sentryDsn = process.env['SENTRY_CLIENT_DSN']
+  protected static override _serverEnvironment: Environment = (() => {
+    if (typeof window === 'undefined') {
+      // Server-side
+      return (process.env['SERVER_ENV'] as Environment) || Environment.Dev
+    } else {
+      // Client-side
+      return (import.meta.env.VITE_SERVER_ENV as Environment) || Environment.Dev
+    }
+  })()
 
   static get contactEmail() {
     return process.env['VITE_CONTACT_EMAIL'] ?? 'contact@mission-transition-ecologique.beta.gouv.fr'
