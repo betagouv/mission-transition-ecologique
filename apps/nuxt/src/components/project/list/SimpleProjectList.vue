934fec866711c71bf94800e86d67aba6eeabbe52
ttdm <thibaut.tezenas@gmail.com>
2025-04-23 10:32:06 +0200

    wip

--- apps/nuxt/src/components/project/list/SimpleProjectList.vue
+++ apps/nuxt/src/components/project/list/SimpleProjectList.vue
@@ -19,6 +19,9 @@
     >
       <ProjectCard
         :project="project"
+        :is-priority-project="isPriorityProject(project)"
+        :is-unique-priority="isUniquePriority"
+        :priority-order="getPriorityOrder(project)"
         class="fr-radius-a--1v fr-card--shadow fr-enlarge-link"
       />
     </li>
@@ -33,6 +36,7 @@
 <script setup lang="ts">
 import { ProjectType } from '@tee/data'
 import { FilterItemKeys } from '@/types'
+import Navigation from '@/tools/navigation'
 
 const { filters } = storeToRefs(useFiltersStore())
 
@@ -41,5 +45,30 @@ interface Props {
   withCounter?: boolean
   withModalFilter?: boolean
 }
-defineProps<Props>()
+const props = defineProps<Props>()
+
+const filtersStore = useFiltersStore()
+
+const priorityProjects = computed(() => {
+  const isHomePage = new Navigation().isHomepage()
+  const hasThemeSelected = filtersStore.hasThemeTypeSelected()
+  const isCompanyDataSelected = useFiltersStore().getCompanyDataSelected().value
+  if (!isCompanyDataSelected || (isHomePage && !hasThemeSelected)) {
+    return undefined
+  }
+  const projectQty = hasThemeSelected ? 1 : 3
+  return props.projectList ? props.projectList.slice(0, projectQty) : undefined
+})
+
+const isUniquePriority = computed(() => {
+  return priorityProjects.value ? priorityProjects.value.length === 1 : false
+})
+
+const isPriorityProject = (project: ProjectType) => {
+  return priorityProjects.value ? priorityProjects.value.includes(project) : false
+}
+
+const getPriorityOrder = (project: ProjectType) => {
+  return priorityProjects.value ? priorityProjects.value.indexOf(project) + 1 : undefined
+}
 </script>
