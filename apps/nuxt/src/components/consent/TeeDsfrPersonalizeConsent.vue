75240c47d2c218f79c4f6500ebcb76e7d59ec052
Yohann Valentin <pro@yohannvalentin.com>
2025-06-02 17:31:52 +0200

    feat: change cookie consent modal title from paragraph to heading

--- apps/nuxt/src/components/consent/TeeDsfrPersonalizeConsent.vue
+++ apps/nuxt/src/components/consent/TeeDsfrPersonalizeConsent.vue
@@ -18,12 +18,12 @@
               />
             </div>
             <div class="fr-modal__content">
-              <p
+              <h3
                 id="fr-consent-modal-title"
                 class="fr-modal__title fr-mb-2w"
               >
                 Panneau de gestion des cookies
-              </p>
+              </h3>
               <div class="fr-consent-manager">
                 <div
                   v-for="cookie in Object.values(cookies || {})"
