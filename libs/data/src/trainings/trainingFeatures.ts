2acf2ddd72b1b21a4737137a60f97cdb3290a072
ttdm <thibaut.tezenas@gmail.com>
2025-07-31 19:09:18 +0200

    improve abstract baserow url; refacto training feature in sub method to gain clarity

--- libs/data/src/trainings/trainingFeatures.ts
+++ libs/data/src/trainings/trainingFeatures.ts
@@ -4,19 +4,26 @@ import { Training } from './types'
 import { TrainingBaserow } from '../common/baserow/trainingBaserow'
 
 export class TrainingFeatures {
-  async loadTrainings(): Promise<Training[]> {
+  async loadTrainings(): Promise<void> {
+    const trainings = await this.getXmlData()
+    await this.updateDatabase(trainings)
+    return
+  }
+
+  private async getXmlData(): Promise<Training[]> {
     const xmlPath = 'https://formations.ademe.fr/tmp/flux_formations_agir.xml'
     const xmlContent = await axios.get(xmlPath).then((res) => res.data)
     const parsed = await parseStringPromise(xmlContent, { mergeAttrs: true, explicitArray: false })
-    const formations: Training[] = parsed.formations.module || []
 
+    return parsed.formations.module || []
+  }
+
+  private async updateDatabase(trainings: Training[]): Promise<void> {
     const trainingBaserow = new TrainingBaserow()
     await trainingBaserow.getAll()
-    for (const formation of formations) {
-      await trainingBaserow.patchOrCreate(formation)
+    for (const training of trainings) {
+      await trainingBaserow.patchOrCreate(training)
       await new Promise((res) => setTimeout(res, 200))
     }
-
-    return []
   }
 }
