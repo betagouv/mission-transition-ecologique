0b8338cd6e9ba7a740e9a9075badc18dced79848
ttdm <thibaut.tezenas@beta.gouv.fr>
2025-09-08 10:52:52 +0200

    feat: improve program long description display (#2165)
    close #2105

--- apps/nuxt/src/components/program/detail/ProgramLongDescription.vue
+++ apps/nuxt/src/components/program/detail/ProgramLongDescription.vue
@@ -7,29 +7,18 @@
     title-class="fr-h4"
   >
     <template #content>
-      <p
-        v-for="(paragraph, idx) in descriptionParagraphs"
-        :key="`long-description-paragraph-${idx}`"
-        :class="`fr-mb-0 fr-pb-${descriptionParagraphs.length - 1 === idx ? '3v' : '0'}`"
-      >
-        {{ paragraph || '&nbsp;' }}
-      </p>
+      <div v-html="Marked.toHtml(program['description longue'])"></div>
     </template>
   </TeeContentBlock>
 </template>
 
 <script setup lang="ts">
-import { computed } from 'vue'
 import type { ProgramTypeForFront } from '@/types'
 import Translation from '@/tools/translation'
+import { Marked } from '@/tools/marked'
 
 interface Props {
   program: ProgramTypeForFront
 }
-const props = defineProps<Props>()
-
-const descriptionParagraphs = computed(() => {
-  const textRaw = props.program['description longue']
-  return textRaw ? textRaw.split('\n') : []
-})
+defineProps<Props>()
 </script>
