48085c00144ff321d769ea5d7eb9dcb7dbbf7fab
Yohann Valentin <pro@yohannvalentin.com>
2025-09-11 17:20:05 +0200

    fix: remove unnecessary inheritance from AbstractBaserow in FaqBaserowMock

--- libs/data/tests/faq/faqBaserow.mock.ts
+++ libs/data/tests/faq/faqBaserow.mock.ts
@@ -1,9 +1,8 @@
-import { AbstractBaserow } from '../../src/common/baserow/abstractBaserow'
 import { BaserowFaq, BaserowFaqSection, FaqBaserowInterface } from '../../src/common/baserow/types'
 import * as faqFixtures from './fixtures/faq.fixtures'
 import * as faqSection from './fixtures/faqSection.fixtures'
 
-export class FaqBaserowMock extends AbstractBaserow implements FaqBaserowInterface {
+export class FaqBaserowMock implements FaqBaserowInterface {
   async getFaqs(): Promise<{ baserowFaqs: BaserowFaq[]; baserowFaqSections: BaserowFaqSection[] }> {
     return {
       baserowFaqs: faqFixtures.faqBaserow as unknown as BaserowFaq[],
