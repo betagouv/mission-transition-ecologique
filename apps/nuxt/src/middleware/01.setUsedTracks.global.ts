2f0ecad708a666379f0b82fc6d5958275b022aea
Yohann Valentin <pro@yohannvalentin.com>
2025-12-10 15:44:50 +0100

    feat: refactor project list component to use computed limit and clean up middleware logs

--- apps/nuxt/src/middleware/01.setUsedTracks.global.ts
+++ apps/nuxt/src/middleware/01.setUsedTracks.global.ts
@@ -1,10 +1,6 @@
 import RouteUtils from '@/middleware/utils/routeUtils'
 
 export default defineNuxtRouteMiddleware(async (to, from) => {
-  console.log(to.query)
-  console.log(RouteUtils.hasQuery(to) && !RouteUtils.hasNameRoute(from))
-  console.log(RouteUtils.hasQuery(to))
-  console.log(!RouteUtils.hasNameRoute(from))
   if (RouteUtils.hasQuery(to) && !RouteUtils.hasNameRoute(from)) {
     useNavigationStore().setSearchParams(to.query)
     await useUsedTrackStore().setFromNavigation()
